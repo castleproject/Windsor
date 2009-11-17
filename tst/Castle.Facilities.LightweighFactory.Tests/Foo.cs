bbc132f4c5259ebb28034249bf1a881451a3376c
xtoff <xtoff@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-11-17 21:18:10 +0000

    - added initial working, sketchy implementation
    git-svn-id: http://subversion.assembla.com/svn/kkozmic/Garage/Castle.LightweightFactoryFacility@62 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ tst/Castle.Facilities.LightweighFactory.Tests/Foo.cs
@@ -0,0 +1,17 @@
+﻿namespace Castle.Facilities.LightweighFactory.Tests
+{
+	public class Foo
+	{
+		private readonly int number;
+
+		public Foo(int arg)
+		{
+			number = arg;
+		}
+
+		public int Number
+		{
+			get { return number; }
+		}
+	}
+}
---
+++ tst/Castle.Facilities.LightweighFactory.Tests/LightweightFactoryTestCase.cs
@@ -0,0 +1,52 @@
+﻿namespace Castle.Facilities.LightweighFactory.Tests
+{
+	using Castle.Core;
+	using Castle.MicroKernel.Resolvers;
+	using Castle.Windsor;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class LightweightFactoryTestCase
+	{
+		#region Setup/Teardown
+
+		[SetUp]
+		public void SetUpTests()
+		{
+			container = new WindsorContainer();
+			// TODO: wrap this with a facility... or nor? We could use ExpressionTreeBasedDelegateBuilder
+			// as default with 'poor mans dependency injection'...
+			container.AddComponent<ILazyComponentLoader, LightweightFactory>("lightweight factory");
+			container.AddComponent<IDelegateBuilder, ExpressionTreeBasedDelegateBuilder>();
+		}
+
+		#endregion
+
+		private WindsorContainer container;
+
+		[Test]
+		public void Can_resolve_service_via_delegate()
+		{
+			container.AddComponentLifeStyle<Foo>("MyFoo", LifestyleType.Transient);
+			container.AddComponent<UsesFooDelegate>();
+			var dependsOnFoo = container.Resolve<UsesFooDelegate>();
+			Foo foo = dependsOnFoo.GetFoo();
+			Assert.AreEqual(1, foo.Number);
+			foo = dependsOnFoo.GetFoo();
+			Assert.AreEqual(2, foo.Number);
+		}
+
+		[Test]
+		public void Delegate_obeys_lifestyle()
+		{
+			container.AddComponentLifeStyle<Foo>("MyFoo", LifestyleType.Singleton);
+			container.AddComponent<UsesFooDelegate>();
+			var dependsOnFoo = container.Resolve<UsesFooDelegate>();
+			Foo foo = dependsOnFoo.GetFoo();
+			Assert.AreEqual(1, foo.Number);
+			foo = dependsOnFoo.GetFoo();
+			Assert.AreEqual(1, foo.Number);
+		}
+	}
+}
