ef7b1fd3d5f797afe34b61bcb7fc23b1ac5a8205
xtoff <xtoff@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-02-22 21:58:55 +0000

    - updated Castle references - minor clean up. This is to set up the stage for further modifications to play nicer with Windsor, and not hack it.
    git-svn-id: http://subversion.assembla.com/svn/kkozmic/Garage/Castle.LightweightFactoryFacility@80 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- Castle.Facilities.LightweighFactory/LightweightFactoryFacility.cs
+++ Castle.Facilities.LightweighFactory/LightweightFactoryFacility.cs
@@ -5,12 +5,21 @@ namespace Castle.Facilities.LightweighFactory
 
 	public class LightweightFactoryFacility : AbstractFacility
 	{
+		public static readonly string FactoryKey = "lightweight-factory";
+		public static readonly string DelegateBuilderKey = "lightweight-factory-delegate-builder";
+
 		protected override void Init()
 		{
 			Kernel.Resolver.AddSubResolver(new ParametersBinder());
-			Kernel.AddComponent("lightweight-factory", typeof(ILazyComponentLoader), typeof(LightweightFactory));
-			Kernel.AddComponent("lightweight-factory-delegate-builder", typeof(IDelegateBuilder),
-								typeof(ExpressionTreeBasedDelegateBuilder));
+			if (!Kernel.HasComponent(FactoryKey))
+			{
+				Kernel.AddComponent(FactoryKey, typeof(ILazyComponentLoader), typeof(LightweightFactory));
+			}
+			if (!Kernel.HasComponent(DelegateBuilderKey))
+			{
+				Kernel.AddComponent(DelegateBuilderKey, typeof(IDelegateBuilder),
+				                    typeof(ExpressionTreeBasedDelegateBuilder));
+			}
 		}
 	}
 }
