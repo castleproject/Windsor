cb5cffb4bffe55f97df1a3356014f706439a0be3
Krzysztof Koźmic <krzysztof@kozmic.pl>
2010-02-22 20:10:26 +0000

    - added new .ctor overloads to ComponentNotFoundException, so that it's possible to pass custom exception message.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/InversionOfControl/trunk@6758 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- src/Castle.MicroKernel/Exceptions/ComponentNotFoundException.cs
+++ src/Castle.MicroKernel/Exceptions/ComponentNotFoundException.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -27,30 +27,65 @@ namespace Castle.MicroKernel
 #endif
 	public class ComponentNotFoundException : Exception
 	{
+		public Type Service { get; private set; }
+		public string Name { get; private set; }
+
 		/// <summary>
-		/// Initializes a new instance of the <see cref="ComponentNotFoundException"/> class.
+		///   Initializes a new instance of the
+		///   <see cref = "ComponentNotFoundException" />
+		///   class.
 		/// </summary>
-		/// <param name="name">The name.</param>
-		public ComponentNotFoundException(String name) : 
-			base( String.Format("No component for key {0} was found", name) )
+		/// <param name = "name">The name.</param>
+		public ComponentNotFoundException(String name) :
+			this(name, String.Format("No component for key {0} was found", name))
 		{
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="ComponentNotFoundException"/> class.
+		///   Initializes a new instance of the
+		///   <see cref = "ComponentNotFoundException" />
+		///   class.
 		/// </summary>
-		/// <param name="service">The service.</param>
-		public ComponentNotFoundException(Type service) : 
-			base( String.Format("No component for supporting the service {0} was found", service.FullName) )
+		/// <param name = "name">The name.</param>
+		/// <param name = "message">Exception message.</param>
+		public ComponentNotFoundException(string name, string message)
+			: base(message)
+		{
+			Name = name;
+		}
+
+		/// <summary>
+		///   Initializes a new instance of the
+		///   <see cref = "ComponentNotFoundException" />
+		///   class.
+		/// </summary>
+		/// <param name = "service">The service.</param>
+		/// <param name = "message">Exception message.</param>
+		public ComponentNotFoundException(Type service, string message)
+			: base(message)
+		{
+			Service = service;
+		}
+
+		/// <summary>
+		///   Initializes a new instance of the
+		///   <see cref = "ComponentNotFoundException" />
+		///   class.
+		/// </summary>
+		/// <param name = "service">The service.</param>
+		public ComponentNotFoundException(Type service) :
+			this(service, String.Format("No component for supporting the service {0} was found", service.FullName))
 		{
 		}
-		
+
 #if (!SILVERLIGHT)
 		/// <summary>
-		/// Initializes a new instance of the <see cref="ComponentNotFoundException"/> class.
+		///   Initializes a new instance of the
+		///   <see cref = "ComponentNotFoundException" />
+		///   class.
 		/// </summary>
-		/// <param name="info">The object that holds the serialized object data.</param>
-		/// <param name="context">The contextual information about the source or destination.</param>
+		/// <param name = "info">The object that holds the serialized object data.</param>
+		/// <param name = "context">The contextual information about the source or destination.</param>
 		public ComponentNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
 		{
 		}
