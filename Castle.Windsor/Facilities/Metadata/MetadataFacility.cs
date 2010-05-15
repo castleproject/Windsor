ef1b93f89e5897a677e6f03cfa8b64fd9ef97662
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-05-15 14:38:29 +0200

    - added initial impl. of metadata facility. At this point this is no prime time ready and more of a proof of concept shape.

---
+++ Castle.Windsor/Facilities/Metadata/MetadataFacility.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.Metadata
+{
+	using System.Linq;
+
+	using Castle.Core;
+	using Castle.DynamicProxy;
+	using Castle.MicroKernel.Facilities;
+	using Castle.MicroKernel.Registration;
+
+	public class MetadataFacility : AbstractFacility
+	{
+		protected MetadataItemInterceptor GetDataInterceptor(object component)
+		{
+			return ((IProxyTargetAccessor)component).GetInterceptors()
+			       	.Single(i => i is MetadataItemInterceptor) as MetadataItemInterceptor;
+		}
+
+		protected bool? GetShouldLoadLazilyProperty(ComponentModel componentModel)
+		{
+			return (bool?)componentModel.ExtendedProperties["metadata.load-lazily"];
+		}
+
+		protected override void Init()
+		{
+			Kernel.ComponentModelBuilder.AddContributor(new MetadataContributor());
+			Kernel.ComponentCreated += PerformLazyInit;
+			Kernel.Register(Component.For<MetadataDataInterceptor>().LifeStyle.Transient,
+			                Component.For<MetadataItemInterceptor>().LifeStyle.Transient);
+		}
+
+		private void PerformLazyInit(ComponentModel model, object instance)
+		{
+			var shouldLoadLazily = GetShouldLoadLazilyProperty(model);
+			if (shouldLoadLazily.HasValue == false)
+			{
+				return;
+			}
+
+			if (shouldLoadLazily.Value == false)
+			{
+				var loader = GetDataInterceptor(instance);
+				loader.LoadComponent();
+			}
+		}
+	}
+}
---
+++ Castle.Windsor/Facilities/Metadata/MetadataRegistration.cs
@@ -0,0 +1,64 @@
+﻿// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.Metadata
+{
+	using System;
+	using System.Collections;
+
+	using Castle.Components.DictionaryAdapter;
+	using Castle.MicroKernel.Registration;
+
+	public static class MetadataRegistration
+	{
+		private static readonly DictionaryAdapterFactory dictionaryAdapterFactory = new DictionaryAdapterFactory();
+
+		public static ComponentRegistration<TComponent> LazyInit<TComponent>(
+			this ComponentRegistration<TComponent> registration)
+		{
+			return registration.ExtendedProperties(
+				Property.ForKey("metadata.load-lazily").Eq(true));
+		}
+
+		public static ComponentRegistration<TComponent> WithMetadata<TComponent, TMetadata>(
+			this ComponentRegistration<TComponent> registration, Action<TMetadata> values)
+		{
+			var metadata = new Hashtable();
+			var forWriting = GetAdapterForWriting<TMetadata>(metadata);
+
+			values(forWriting);
+
+			var forReading = GetAdapterForReading<TMetadata>(metadata);
+
+			return registration.AddDescriptor(new ExtendedPropertiesDescriptor<TComponent>(metadata))
+				.ExtendedProperties(Property.ForKey("metadata.metadata-type").Eq(typeof(TMetadata)),
+				                    Property.ForKey("metadata.metadata-adapter").Eq(forReading));
+		}
+
+		private static TMetadata GetAdapterForReading<TMetadata>(IDictionary metadata)
+		{
+			var builder = new PropertyDescriptor()
+				.AddKeyBuilder(new KeyPrefixAttribute("metadata."))
+				.AddSetter(new NoOpSetter());
+			return (TMetadata)dictionaryAdapterFactory.GetAdapter(typeof(TMetadata), metadata, builder);
+		}
+
+		private static TMetadata GetAdapterForWriting<TMetadata>(IDictionary metadata)
+		{
+			var builder = new PropertyDescriptor()
+				.AddKeyBuilder(new KeyPrefixAttribute("metadata."));
+			return (TMetadata)dictionaryAdapterFactory.GetAdapter(typeof(TMetadata), metadata, builder);
+		}
+	}
+}
