8571c539717ce883c1add4e6c6c626d437c67ddd
Craig Neuwirt <cneuwirt@gmail.com>
2010-07-06 20:40:53 -0500

    Added Castle.Facilities.Synchronize to Castle.InversionOfControl

---
+++ src/Castle.Facilities.Synchronize/CreateOnUIThreadActivator.cs
@@ -0,0 +1,91 @@
+// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.Synchronize
+{
+	using System;
+	using Castle.Core;
+	using Castle.MicroKernel;
+	using Castle.MicroKernel.ComponentActivator;
+	using Castle.MicroKernel.Context;
+	
+	internal delegate object CreationContextDelegate(CreationContext context);
+
+	internal delegate object CreateOnUIThreadDelegate(CreationContextDelegate create, CreationContext context);
+
+	internal class CreateOnUIThreadActivator : DefaultComponentActivator
+	{
+		private readonly IComponentActivator customActivator;
+		private readonly CreationContextDelegate performCreation;
+
+		static readonly ComponentInstanceDelegate emptyCreation = delegate { };
+		static readonly ComponentInstanceDelegate emptyDestruction = delegate { };
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CreateOnUIThreadActivator"/> class.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <param name="kernel">The kernel.</param>
+		/// <param name="onCreation">Delegate called on construction.</param>
+		/// <param name="onDestruction">Delegate called on destruction.</param>
+		public CreateOnUIThreadActivator(ComponentModel model, IKernel kernel,
+		                                 ComponentInstanceDelegate onCreation,
+		                                 ComponentInstanceDelegate onDestruction)
+			: base(model, kernel, onCreation, onDestruction)
+		{
+			customActivator = CreateCustomActivator(model, kernel);
+			performCreation = PerformCreation;
+		}
+
+		protected override object InternalCreate(CreationContext context)
+		{
+			var createOnUIThread = (CreateOnUIThreadDelegate)Model.ExtendedProperties[Constants.CreateOnUIThead];
+
+			if (createOnUIThread != null)
+			{
+				return createOnUIThread(performCreation, context);
+			}
+
+			return base.InternalCreate(context);
+		}
+
+		private object PerformCreation(CreationContext context)
+		{
+			if (customActivator != null) 
+			{
+				return customActivator.Create(context);
+			}
+
+			return base.InternalCreate(context);
+		}
+
+		private static IComponentActivator CreateCustomActivator(ComponentModel model, IKernel kernel)
+		{
+			var customActivator = (Type)model.ExtendedProperties[Constants.CustomActivator];
+			if (customActivator != null)
+			{
+				try
+				{
+					return (IComponentActivator)Activator.CreateInstance(
+						customActivator,model, kernel, emptyCreation, emptyDestruction);
+				}
+				catch (Exception e)
+				{
+					throw new KernelException("Could not instantiate the custom activator", e);
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ src/Castle.Facilities.Synchronize/CreateOnUIThreadInspector.cs
@@ -0,0 +1,234 @@
+// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.Synchronize
+{
+	using System;
+	using System.Configuration;
+	using System.Runtime.Remoting;
+	using System.Threading;
+	using System.Windows;
+	using System.Windows.Forms;
+	using System.Windows.Threading;
+	using Castle.Core;
+	using Castle.Core.Configuration;
+	using Castle.DynamicProxy;
+	using Castle.MicroKernel;
+	using Castle.MicroKernel.Context;
+	using Castle.MicroKernel.ModelBuilder;
+	using Castle.MicroKernel.Proxy;
+	using Castle.MicroKernel.Registration;
+	using Castle.MicroKernel.SubSystems.Conversion;
+	using Castle.MicroKernel.Util;
+
+	/// <summary>
+	/// Ensure that synchronized controls are always created on the main Ui thread.
+	/// </summary>
+	internal class CreateOnUIThreadInspector : IContributeComponentModelConstruction, IDisposable
+	{
+		private readonly MarshalingControl marshalingControl;
+		private readonly IReference<IProxyGenerationHook> controlProxyHook;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CreateOnUIThreadInspector"/> class.
+		/// </summary>
+		/// <param name="kernel">The kernel.</param>
+		/// <param name="config">The config.</param>
+		public CreateOnUIThreadInspector(IKernel kernel, IConfiguration config)
+		{
+			marshalingControl = new MarshalingControl();
+			controlProxyHook = ObtainProxyHook(kernel, config);
+			RegisterAmbientSynchronizationContext<WindowsFormsSynchronizationContext>(kernel);
+			RegisterAmbientSynchronizationContext<DispatcherSynchronizationContext>(kernel);
+			kernel.ComponentModelCreated += Kernel_ComponentModelCreated;
+		}
+
+		/// <summary>
+		/// Processes <see cref="Control"/> implementations.
+		/// </summary>
+		/// <param name="kernel">The kernel.</param>
+		/// <param name="model">The model.</param>
+		public void ProcessModel(IKernel kernel, ComponentModel model)
+		{
+			CreateOnUIThreadDelegate createOnUIThread = null;
+
+			// Since controls created on different threads cannot be added to one
+			// another, ensure that all controls are created on the main UI thread.
+
+			if (typeof(Control).IsAssignableFrom(model.Implementation))
+			{
+				createOnUIThread = CreateOnWinformsUIThread;
+			}
+			else if (typeof(UIElement).IsAssignableFrom(model.Implementation))
+			{
+				createOnUIThread = CreateOnDispatcherUIThread;
+			}
+
+			if (createOnUIThread != null)
+			{
+				ConfigureProxyOptions(model);
+				model.ExtendedProperties[Constants.CreateOnUIThead] = createOnUIThread;
+			}
+		}
+
+		private static void Kernel_ComponentModelCreated(ComponentModel model)
+		{
+			if (model.ExtendedProperties.Contains(Constants.CreateOnUIThead))
+			{
+				if (model.CustomComponentActivator != null)
+					model.ExtendedProperties[Constants.CustomActivator] = model.CustomComponentActivator;
+				model.CustomComponentActivator = typeof(CreateOnUIThreadActivator);
+			}
+		}
+
+		private object CreateOnWinformsUIThread(CreationContextDelegate performCreation, CreationContext context)
+		{
+			if (marshalingControl.InvokeRequired)
+			{
+				return marshalingControl.Invoke((CreateOnUIThreadDelegate)CreateOnWinformsUIThread,
+					new object[] { performCreation, context });
+			}
+
+			var component = performCreation(context);
+			var control = (Control)GetUnproxiedInstance(component);
+			if (control.Handle == IntPtr.Zero)
+			{
+				throw new InvalidOperationException("The WinForms control handle could not be obtained");
+			}
+			return component;
+		}
+
+		private static object CreateOnDispatcherUIThread(CreationContextDelegate performCreation, CreationContext context)
+		{
+			var application = System.Windows.Application.Current;
+
+			if (application != null && application.CheckAccess() == false)
+			{
+				return application.Dispatcher.Invoke((CreateOnUIThreadDelegate)
+					CreateOnDispatcherUIThread, performCreation, context);
+			}
+
+			return performCreation(context);
+		}
+
+		private void ConfigureProxyOptions(ComponentModel model)
+		{
+			if (controlProxyHook != null)
+			{
+				var proxyOptions = ProxyUtil.ObtainProxyOptions(model, true);
+				proxyOptions.Hook = controlProxyHook;
+			}
+		}
+
+		private static IReference<IProxyGenerationHook> ObtainProxyHook(IKernel kernel, IConfiguration config)
+		{
+			IProxyGenerationHook hook = null;
+
+			if (config != null)
+			{
+				var hookAttrib = config.Attributes[Constants.ControlProxyHookAttrib];
+
+				if (hookAttrib != null)
+				{
+					if (ReferenceExpressionUtil.IsReference(hookAttrib))
+					{
+						var hookKey = ReferenceExpressionUtil.ExtractComponentKey(hookAttrib);
+						return new ComponentReference<IProxyGenerationHook>(hookKey);
+					}
+
+					var converter = (ITypeConverter)kernel.GetSubSystem(SubSystemConstants.ConversionManagerKey);
+
+					var hookType = (Type) converter.PerformConversion(hookAttrib, typeof(Type));
+
+					if (!typeof(IProxyGenerationHook).IsAssignableFrom(hookType))
+					{
+						var message = String.Format("The specified controlProxyHook does " +
+						                            "not implement the interface IProxyGenerationHook. Type {0}",
+						                            hookType.FullName);
+
+						throw new ConfigurationErrorsException(message);
+					}
+
+					hook = (IProxyGenerationHook)Activator.CreateInstance(hookType);
+				}
+			}
+
+			if (hook == null)
+			{
+				hook = SynchronizeProxyHook.Instance;
+			}
+
+			return new InstanceReference<IProxyGenerationHook>(hook);
+		}
+
+		private static void RegisterAmbientSynchronizationContext<C>(IKernel kernel)
+			where C : SynchronizationContext
+		{
+			var syncContext = SynchronizationContext.Current as C;
+
+			if (syncContext != null)
+			{
+				kernel.Register(Component.For<C>().Instance(syncContext));
+			}
+		}
+
+		private static object GetUnproxiedInstance(object instance)
+		{
+			if (!RemotingServices.IsTransparentProxy(instance))
+			{
+				var accessor = instance as IProxyTargetAccessor;
+
+				if (accessor != null)
+				{
+					instance = accessor.DynProxyGetTarget();
+				}
+			}
+			return instance;
+		}
+
+		/// <summary>
+		/// Releases the marshaling control.
+		/// </summary>
+		void IDisposable.Dispose()
+		{
+			if (marshalingControl != null)
+			{
+				marshalingControl.Dispose();
+			}
+		}
+
+		#region MarshalingControl
+
+		private class MarshalingControl : Control
+		{
+			internal MarshalingControl()
+			{
+				Visible = false;
+				SetTopLevel(true);
+				CreateControl();
+				CreateHandle();
+			}
+
+			protected override void OnLayout(LayoutEventArgs levent)
+			{
+			}
+
+			protected override void OnSizeChanged(EventArgs e)
+			{
+			}
+		}
+
+		#endregion
+	}
+}
