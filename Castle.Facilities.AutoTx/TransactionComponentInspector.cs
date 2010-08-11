a969bec555600a165129d88252bf266451cf84b9
hconceicao <henry.conceicao@gmail.com>
2010-08-11 00:33:49 -0300

    updated license headers and minor code reformat

--- Castle.Facilities.AutoTx/TransactionComponentInspector.cs
+++ Castle.Facilities.AutoTx/TransactionComponentInspector.cs
@@ -1,29 +1,31 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+#region License
+//  Copyright 2004-2010 Castle Project - http:www.castleproject.org/
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http:www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 // 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+#endregion
 namespace Castle.Facilities.AutoTx
 {
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
 	using System.Reflection;
-	using Castle.Core;
-	using Castle.MicroKernel;
-	using Castle.MicroKernel.ModelBuilder.Inspectors;
-	using Castle.MicroKernel.Facilities;
-	using Castle.Core.Configuration;
-	using Castle.Services.Transaction;
+	using Core;
+	using Core.Configuration;
+	using MicroKernel;
+	using MicroKernel.Facilities;
+	using MicroKernel.ModelBuilder.Inspectors;
+	using Services.Transaction;
 
 	/// <summary>
 	/// Tries to obtain transaction configuration based on 
