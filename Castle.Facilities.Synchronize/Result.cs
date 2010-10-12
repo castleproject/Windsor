d00488d3fd94bdee4f6b763a625a633b4220d084
flcdrg <david@gardiner.net.au>
2010-10-12 15:05:39 +0800

    Fix xml comment spelling in Castle.Facilities.Synchronize

--- Castle.Facilities.Synchronize/Result.cs
+++ Castle.Facilities.Synchronize/Result.cs
@@ -225,7 +225,7 @@ namespace Castle.Facilities.Synchronize
 		#region IAsyncResult Members
 
 		/// <summary>
-		/// Gets the asynchournous state.
+		/// Gets the asynchronous state.
 		/// </summary>
 		public object AsyncState
 		{
