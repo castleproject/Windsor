8a891dd8025a18b0f89b6b51e83e41aa645bba8d
hconceicao <henry.conceicao@gmail.com>
2010-11-23 17:05:32 -0200

    some work for the facility fluent config

---
+++ Castle.Facilities.NHibernateIntegration.Tests/FacilityFluentConfigTestCase.cs
@@ -0,0 +1,34 @@
+﻿namespace Castle.Facilities.NHibernateIntegration.Tests
+{
+	using Core.Resource;
+	using MicroKernel.Facilities;
+	using NUnit.Framework;
+	using Windsor;
+	using Windsor.Configuration.Interpreters;
+
+	[TestFixture]
+	public class FacilityFluentConfigTestCase
+	{
+		[Test]
+		public void Should_override_DefaultConfigurationBuilder()
+		{
+			var file = "Castle.Facilities.NHibernateIntegration.Tests/MinimalConfiguration.xml";
+
+			var container = new WindsorContainer(new XmlInterpreter(new AssemblyResource(file)));
+
+			container.AddFacility<NHibernateFacility>("nhibernatefacility", f => f.CustomConfigurationBuilder = typeof(TestConfigurationBuilder));
+
+			Assert.AreEqual(typeof(TestConfigurationBuilder), container.Resolve<IConfigurationBuilder>().GetType());
+		}
+
+		[Test, ExpectedException(typeof(FacilityException))]
+		public void Should_not_accept_non_implementors_of_IConfigurationBuilder_for_override()
+		{
+			var file = "Castle.Facilities.NHibernateIntegration.Tests/MinimalConfiguration.xml";
+
+			var container = new WindsorContainer(new XmlInterpreter(new AssemblyResource(file)));
+
+			container.AddFacility<NHibernateFacility>("nhibernatefacility", f => f.CustomConfigurationBuilder = GetType());
+		}
+	}
+}
