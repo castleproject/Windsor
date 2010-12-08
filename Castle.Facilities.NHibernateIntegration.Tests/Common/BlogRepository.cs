b7f3ab9ab6a17ae883a2f6ecada83e3850461ca6
hconceicao <henry.conceicao@gmail.com>
2010-12-08 16:15:43 -0200

    nhsession attributes and interceptors

---
+++ Castle.Facilities.NHibernateIntegration.Tests/Common/BlogRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Castle.Facilities.NHibernateIntegration.Tests.Common
+{
+	using NUnit.Framework;
+
+	[NHSessionAware]
+	public class BlogRepository
+	{
+		private readonly ISessionStore sessionStore;
+		private readonly ISessionManager sessionManager;
+
+		public BlogRepository(ISessionManager sessionManager, ISessionStore sessionStore)
+		{
+			this.sessionStore = sessionStore;
+			this.sessionManager = sessionManager;
+		}
+
+		[NHSessionRequired]
+		public virtual void FetchAll()
+		{
+			Assert.IsNotNull(sessionStore.FindCompatibleSession(Constants.DefaultAlias));
+
+			sessionManager.OpenSession();
+		}
+	}
+}
