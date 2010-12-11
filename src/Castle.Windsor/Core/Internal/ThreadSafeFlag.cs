3e535429d1a515614fe44a1ea6ceaadec0838c20
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-12-11 15:26:43 +1000

    - simplified SingletonLifestyleManager a bit. Removed volatile from the field definition. There's no reason why it should be volatile - all multithreaded access is already within a lock, and Dispose is now guaranteed to be invoked only once
    - added check on the DefaultKernel to only ever Dispose handlers once. This lifts the need for multithreaded code in the handler's Dispose
    - added ThreadSafeFlag which pretty much does what it says. It's used to perform lockless, thread safe "only once" operations.

---
+++ src/Castle.Windsor/Core/Internal/ThreadSafeFlag.cs
@@ -0,0 +1,35 @@
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Core.Internal
+{
+	using System.Threading;
+
+	public struct ThreadSafeFlag
+	{
+		/// <summary>
+		///   0 == false, 1 = =true
+		/// </summary>
+		private int signaled;
+
+		/// <summary>
+		/// Signals (sets) the flag.
+		/// </summary>
+		/// <returns><c>true</c> if the current thread signaled the flag, <c>false</c> if some other thread signaled the flag before.</returns>
+		public bool Signal()
+		{
+			return Interlocked.Exchange(ref signaled, 1) == 0;
+		}
+	}
+}
