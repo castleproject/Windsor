68ef19b24097cdb51f970f57f0d67ad7ae9d97fe
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-12-15 23:34:21 +1000

    - fixes for broken CP and SL builds

--- src/Castle.Windsor/MicroKernel/Lifestyle/PerThreadThreadStaticLifestyleManager.cs
+++ src/Castle.Windsor/MicroKernel/Lifestyle/PerThreadThreadStaticLifestyleManager.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 namespace Castle.MicroKernel.Lifestyle
 {
 #if (SILVERLIGHT)
@@ -22,56 +21,67 @@ namespace Castle.MicroKernel.Lifestyle
 	using Castle.MicroKernel.Context;
 
 	/// <summary>
-	/// per thread LifestyleManager implementation compatible with Silverlight.
+	///   per thread LifestyleManager implementation compatible with Silverlight.
 	/// </summary>
 	public class PerThreadThreadStaticLifestyleManager : AbstractLifestyleManager
 	{
 		[ThreadStatic]
-		private static Dictionary<IComponentActivator, object> map;
+		private static Dictionary<IComponentActivator, Burden> map;
 
-		public static Dictionary<IComponentActivator, object> Map
+		/// <summary>
+		/// </summary>
+		public override void Dispose()
+		{
+			if (map == null)
+			{
+				return;
+			}
+
+			var dictionary = Map;
+			Burden burden;
+			if (dictionary.TryGetValue(ComponentActivator, out burden))
+			{
+				map.Remove(ComponentActivator);
+				burden.Release();
+			}
+		}
+
+		public override object Resolve(CreationContext context, Burden burden, IReleasePolicy releasePolicy)
+		{
+			Burden cachedBurden;
+
+			var dictionary = Map;
+			if (dictionary.TryGetValue(ComponentActivator, out cachedBurden))
+			{
+				return burden.Instance;
+			}
+			var instance = base.Resolve(context, burden, releasePolicy);
+			dictionary.Add(ComponentActivator, burden);
+
+			return instance;
+		}
+
+		protected override void Track(Burden burden, IReleasePolicy releasePolicy)
+		{
+			var track = burden.RequiresPolicyRelease;
+			burden.RequiresPolicyRelease = false;
+			if (track)
+			{
+				releasePolicy.Track(burden.Instance, burden);
+			}
+		}
+
+		public static Dictionary<IComponentActivator, Burden> Map
 		{
 			get
 			{
 				if (map == null)
 				{
-					map = new Dictionary<IComponentActivator, object>();
+					map = new Dictionary<IComponentActivator, Burden>();
 				}
 				return map;
 			}
 		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public override void Dispose()
-		{
-			foreach (var instance in Map.Values)
-			{
-				base.Release(instance);
-			}
-		}
-
-		public override object Resolve(CreationContext context)
-		{
-			Object instance;
-
-			var dictionary = Map;
-			if (!dictionary.TryGetValue(ComponentActivator, out instance))
-			{
-				instance = base.Resolve(context);
-				dictionary.Add(ComponentActivator, instance);
-			}
-
-			return instance;
-		}
-
-		public override bool Release(object instance)
-		{
-			// Do nothing.
-			return false;
-		}
-
 	}
 #endif
 }
