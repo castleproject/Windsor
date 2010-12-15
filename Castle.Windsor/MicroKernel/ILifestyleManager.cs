2524a6f0f54c13ece94d3989aa87c3bf33079ad2
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-12-15 23:25:15 +1000

    - reformatted ILifestyleManager

--- Castle.Windsor/MicroKernel/ILifestyleManager.cs
+++ Castle.Windsor/MicroKernel/ILifestyleManager.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,48 +15,49 @@
 namespace Castle.MicroKernel
 {
 	using System;
+
 	using Castle.Core;
 	using Castle.MicroKernel.Context;
 
 	/// <summary>
-	/// The <c>ILifestyleManager</c> implements 
-	/// a strategy for a given lifestyle, like singleton, per-thread
-	/// and transient.
+	///   The <c>ILifestyleManager</c> implements 
+	///   a strategy for a given lifestyle, like singleton, per-thread
+	///   and transient.
 	/// </summary>
 	/// <remarks>
-	/// The responsibility of <c>ILifestyleManager</c>
-	/// is only the management of lifestyle. It should rely on
-	/// <see cref="IComponentActivator"/> to obtain a new component instance
+	///   The responsibility of <c>ILifestyleManager</c>
+	///   is only the management of lifestyle. It should rely on
+	///   <see cref = "IComponentActivator" /> to obtain a new component instance
 	/// </remarks>
 	public interface ILifestyleManager : IDisposable
 	{
 		/// <summary>
-		/// Initializes the <c>ILifestyleManager</c> with the 
-		/// <see cref="IComponentActivator"/>
+		///   Initializes the <c>ILifestyleManager</c> with the 
+		///   <see cref = "IComponentActivator" />
 		/// </summary>
-		/// <param name="componentActivator"></param>
-		/// <param name="kernel"></param>
-		/// <param name="model"></param>
+		/// <param name = "componentActivator"></param>
+		/// <param name = "kernel"></param>
+		/// <param name = "model"></param>
 		void Init(IComponentActivator componentActivator, IKernel kernel, ComponentModel model);
 
 		/// <summary>
-		/// Implementors should return the component instance based on the lifestyle semantic.
-		/// Also the instance should be set to <see cref="Burden.SetRootInstance"/>, <see cref="Burden.RequiresPolicyRelease"/> should be also set if needed
-		/// and if a new instance was created it should be passed on to <see cref="IReleasePolicy.Track"/> of <paramref name="releasePolicy"/>.
+		///   Implementors should release the component instance based
+		///   on the lifestyle semantic, for example, singleton components
+		///   should not be released on a call for release, instead they should
+		///   release them when disposed is invoked.
 		/// </summary>
-		/// <param name="context" />
-		/// <param name="burden" />
-		/// <param name="releasePolicy" />
-		/// <returns></returns>
-		object Resolve(CreationContext context, Burden burden, IReleasePolicy releasePolicy);
+		/// <param name = "instance"></param>
+		bool Release(object instance);
 
 		/// <summary>
-		/// Implementors should release the component instance based
-		/// on the lifestyle semantic, for example, singleton components
-		/// should not be released on a call for release, instead they should
-		/// release them when disposed is invoked.
+		///   Implementors should return the component instance based on the lifestyle semantic.
+		///   Also the instance should be set to <see cref = "Burden.SetRootInstance" />, <see cref = "Burden.RequiresPolicyRelease" /> should be also set if needed
+		///   and if a new instance was created it should be passed on to <see cref = "IReleasePolicy.Track" /> of <paramref name = "releasePolicy" />.
 		/// </summary>
-		/// <param name="instance"></param>
-		bool Release(object instance);
+		/// <param name = "context" />
+		/// <param name = "burden" />
+		/// <param name = "releasePolicy" />
+		/// <returns></returns>
+		object Resolve(CreationContext context, Burden burden, IReleasePolicy releasePolicy);
 	}
 }
