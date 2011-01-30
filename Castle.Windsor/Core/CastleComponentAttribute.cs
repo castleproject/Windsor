afb1777e66ecd705ec3b9d9e1c07c289634ec0d9
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-01-30 11:02:35 +1000

    - changed CastleComponentAttribute to allow defining multiple services

--- Castle.Windsor/Core/CastleComponentAttribute.cs
+++ Castle.Windsor/Core/CastleComponentAttribute.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,44 +17,51 @@ namespace Castle.Core
 	using System;
 
 	/// <summary>
-	/// This attribute is useful only when you want to register all components
-	/// on an assembly as a batch process. 
-	/// By doing so, the batch register will look 
-	/// for this attribute to distinguish components from other classes.
+	///   This attribute is useful only when you want to register all components
+	///   on an assembly as a batch process. 
+	///   By doing so, the batch register will look 
+	///   for this attribute to distinguish components from other classes.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 	public class CastleComponentAttribute : LifestyleAttribute
 	{
-		public CastleComponentAttribute(String key) : this(key, null)
+		public CastleComponentAttribute(String name) : this(name, null)
 		{
 		}
 
-		public CastleComponentAttribute(Type service)
-			: this(null, service)
-		{
-		}
-		public CastleComponentAttribute(String key, Type service) : this(key, service, LifestyleType.Undefined)
+		public CastleComponentAttribute(params Type[] services)
+			: this(null, services)
 		{
 		}
 
-		public CastleComponentAttribute(String key, Type service, LifestyleType lifestyle) : base(lifestyle)
+		public CastleComponentAttribute(String name, params Type[] services)
+			: this(name, LifestyleType.Undefined, services)
 		{
-			Key = key;
-			Service = service;
 		}
 
-		public String Key { get; private set; }
+		public CastleComponentAttribute(String name, LifestyleType lifestyle, params Type[] services) : base(lifestyle)
+		{
+			Name = name;
+			Services = services ?? Type.EmptyTypes;
+		}
 
-		public Type Service { get; private set; }
+		public bool HasName
+		{
+			get { return string.IsNullOrEmpty(Name) == false; }
+		}
+
+		public String Name { get; private set; }
+
+		public Type[] Services { get; private set; }
 
 		public static CastleComponentAttribute GetDefaultsFor(Type type)
 		{
 			var attribute = (CastleComponentAttribute)GetCustomAttribute(type, typeof(CastleComponentAttribute));
 			if (attribute != null)
 			{
-				if(attribute.Service == null)
+				if (attribute.Services.Length == 0)
 				{
-					attribute.Service = type;
+					attribute.Services = new[] { type };
 				}
 				return attribute;
 			}
