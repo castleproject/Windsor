6d8f9465944d384590fe0b05088b778aa3811213
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-01-30 11:02:35 +1000

    - changed CastleComponentAttribute to allow defining multiple services

--- src/Castle.Windsor.Tests/ComponentsWithAttribute/HasKeyAndTypeTransient.cs
+++ src/Castle.Windsor.Tests/ComponentsWithAttribute/HasKeyAndTypeTransient.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ namespace Castle.Windsor.Tests.ComponentsWithAttribute
 	using Castle.Core;
 	using Castle.Windsor.Tests.Components;
 
-	[CastleComponent("keyAndTypeTransient", typeof(ISimpleService), LifestyleType.Transient)]
+	[CastleComponent("keyAndTypeTransient", LifestyleType.Transient, typeof(ISimpleService))]
 	public class HasKeyAndTypeTransient : ISimpleService
 	{
 		public void Operation()
