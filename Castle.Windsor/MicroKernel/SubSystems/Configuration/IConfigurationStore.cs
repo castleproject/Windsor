eb0120fe4f9d6e200f115c619e3a4cb500e907e5
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-02-01 23:33:59 +1000

    - reformatting

--- Castle.Windsor/MicroKernel/SubSystems/Configuration/IConfigurationStore.cs
+++ Castle.Windsor/MicroKernel/SubSystems/Configuration/IConfigurationStore.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,91 +20,90 @@ namespace Castle.MicroKernel.SubSystems.Configuration
 	using Castle.Core.Resource;
 
 	/// <summary>
-	/// The contract used by the kernel to obtain
-	/// external configuration for the components and
-	/// facilities.
+	///   The contract used by the kernel to obtain
+	///   external configuration for the components and
+	///   facilities.
 	/// </summary>
 	public interface IConfigurationStore : ISubSystem
 	{
 		/// <summary>
-		/// Associates a configuration node with a facility key
+		///   Adds the child container configuration.
 		/// </summary>
-		/// <param name="key">item key</param>
-		/// <param name="config">Configuration node</param>
-		void AddFacilityConfiguration(String key, IConfiguration config);
+		/// <param name = "name">The container's name.</param>
+		/// <param name = "config">The config.</param>
+		void AddChildContainerConfiguration(String name, IConfiguration config);
 
 		/// <summary>
-		/// Associates a configuration node with a component key
+		///   Associates a configuration node with a component key
 		/// </summary>
-		/// <param name="key">item key</param>
-		/// <param name="config">Configuration node</param>
+		/// <param name = "key">item key</param>
+		/// <param name = "config">Configuration node</param>
 		void AddComponentConfiguration(String key, IConfiguration config);
 
 		/// <summary>
-		/// Adds the child container configuration.
+		///   Associates a configuration node with a facility key
 		/// </summary>
-		/// <param name="name">The container's name.</param>
-		/// <param name="config">The config.</param>
-		void AddChildContainerConfiguration(String name, IConfiguration config);
+		/// <param name = "key">item key</param>
+		/// <param name = "config">Configuration node</param>
+		void AddFacilityConfiguration(String key, IConfiguration config);
 
 		void AddInstallerConfiguration(IConfiguration config);
 
 		/// <summary>
-		/// Returns the configuration node associated with 
-		/// the specified child container key. Should return null
-		/// if no association exists.
+		///   Returns the configuration node associated with 
+		///   the specified child container key. Should return null
+		///   if no association exists.
 		/// </summary>
-		/// <param name="key">item key</param>
+		/// <param name = "key">item key</param>
 		/// <returns></returns>
 		IConfiguration GetChildContainerConfiguration(String key);
 
 		/// <summary>
-		/// Returns the configuration node associated with 
-		/// the specified facility key. Should return null
-		/// if no association exists.
+		///   Returns the configuration node associated with 
+		///   the specified component key. Should return null
+		///   if no association exists.
 		/// </summary>
-		/// <param name="key">item key</param>
-		/// <returns></returns>
-		IConfiguration GetFacilityConfiguration(String key);
-
-		/// <summary>
-		/// Returns the configuration node associated with 
-		/// the specified component key. Should return null
-		/// if no association exists.
-		/// </summary>
-		/// <param name="key">item key</param>
+		/// <param name = "key">item key</param>
 		/// <returns></returns>
 		IConfiguration GetComponentConfiguration(String key);
 
 		/// <summary>
-		/// Returns all configuration nodes for facilities
-		/// </summary>
-		/// <returns></returns>
-		IConfiguration[] GetFacilities();
-
-		/// <summary>
-		/// Returns all configuration nodes for components
+		///   Returns all configuration nodes for components
 		/// </summary>
 		/// <returns></returns>
 		IConfiguration[] GetComponents();
 
 		/// <summary>
-		/// Returns all configuration nodes for installers
-		/// </summary>
-		/// <returns></returns>
-		IConfiguration[] GetInstallers();
-
-		/// <summary>
-		/// Gets the child containers configuration nodes.
+		///   Gets the child containers configuration nodes.
 		/// </summary>
 		/// <returns></returns>
 		IConfiguration[] GetConfigurationForChildContainers();
 
 		/// <summary>
-		/// 
+		///   Returns all configuration nodes for facilities
 		/// </summary>
-		/// <param name="resourceUri"></param>
-		/// <param name="resource"></param>
+		/// <returns></returns>
+		IConfiguration[] GetFacilities();
+
+		/// <summary>
+		///   Returns the configuration node associated with 
+		///   the specified facility key. Should return null
+		///   if no association exists.
+		/// </summary>
+		/// <param name = "key">item key</param>
+		/// <returns></returns>
+		IConfiguration GetFacilityConfiguration(String key);
+
+		/// <summary>
+		///   Returns all configuration nodes for installers
+		/// </summary>
+		/// <returns></returns>
+		IConfiguration[] GetInstallers();
+
+		/// <summary>
+		/// </summary>
+		/// <param name = "resourceUri"></param>
+		/// <param name = "resource"></param>
 		/// <returns></returns>
 		IResource GetResource(String resourceUri, IResource resource);
 	}
