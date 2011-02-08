1b92fe6712952e39f9da2a6271f81153b1dba8b8
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-02-08 19:24:54 +1000

    - added scoped lifestyle implementation based on my old proof of concept and then improved by Craig

---
+++ src/Castle.Windsor/MicroKernel/Lifestyle/Scoped/WebScopeAccessor.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.MicroKernel.Lifestyle.Scoped
+{
+#if !(SILVERLIGHT || CLIENTPROFILE)
+	using System.Collections.Generic;
+	using System.Web;
+
+	public class WebScopeAccessor : IScopeAccessor
+	{
+		private const string Scopes = "ioc.scope.currentstack";
+
+		public Stack<LifestyleScope> CurrentStack
+		{
+			get
+			{
+				var current = HttpContext.Current;
+				if (current == null)
+				{
+					return null;
+				}
+
+				var stack = (Stack<LifestyleScope>)current.Items[Scopes];
+
+				if (stack == null)
+				{
+					stack = new Stack<LifestyleScope>();
+					current.Items[Scopes] = stack;
+				}
+
+				return stack;
+			}
+		}
+	}
+#endif
+}
