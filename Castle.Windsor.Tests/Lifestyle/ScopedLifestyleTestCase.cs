763bdfe45e3dd45a11f8e73048831f2cfa4efef7
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-02-23 13:08:03 +1000

    - morrrrr tests

--- Castle.Windsor.Tests/Lifestyle/ScopedLifestyleTestCase.cs
+++ Castle.Windsor.Tests/Lifestyle/ScopedLifestyleTestCase.cs
@@ -19,6 +19,7 @@ namespace Castle.Lifestyle
 	using Castle.MicroKernel.Registration;
 	using Castle.Windsor;
 	using Castle.Windsor.Tests;
+	using Castle.Windsor.Tests.ClassComponents;
 
 	using NUnit.Framework;
 
@@ -29,6 +30,21 @@ namespace Castle.Lifestyle
 			var container = new WindsorContainer();
 			container.Kernel.AddSubSystem("scope", new ScopeSubsystem(new ThreadScopeAccessor()));
 			return container;
+		}
+
+		[Test]
+		public void Ending_scope_releases_component()
+		{
+			DisposableFoo.DisposedCount = 0;
+
+			Container.Register(Component.For<DisposableFoo>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				Container.Resolve<DisposableFoo>();
+			}
+
+			Assert.AreEqual(1, DisposableFoo.DisposedCount);
 		}
 
 		[Test]
@@ -52,6 +68,87 @@ namespace Castle.Lifestyle
 			Assert.AreEqual(
 				"Component 'Castle.Windsor.Tests.A' has scoped lifestyle, and it could not be resolved because no scope is accessible.  Did you forget to call container.BeginScope()?",
 				exception.Message);
+		}
+
+		[Test]
+		[Ignore("This fails... not sure what the behavior should be... that has to be discussed based on some real life usages")]
+		public void Scoped_component_instance_from_outer_scope_is_reused_within_nested_scope()
+		{
+			Container.Register(Component.For<A>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				var a1 = Container.Resolve<A>();
+				using (Container.BeginScope())
+				{
+					var a2 = Container.Resolve<A>();
+					Assert.AreSame(a1, a2);
+				}
+			}
+		}
+
+		[Test]
+		public void Scoped_component_instance_is_reused_within_the_scope()
+		{
+			Container.Register(Component.For<A>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				var a1 = Container.Resolve<A>();
+				var a2 = Container.Resolve<A>();
+				Assert.AreSame(a1, a2);
+			}
+		}
+
+		[Test]
+		public void Scoped_component_is_bound_to_the_innermost_scope()
+		{
+			DisposableFoo.DisposedCount = 0;
+
+			Container.Register(Component.For<DisposableFoo>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				using (Container.BeginScope())
+				{
+					Container.Resolve<DisposableFoo>();
+					Assert.AreEqual(0, DisposableFoo.DisposedCount);
+				}
+				Assert.AreEqual(1, DisposableFoo.DisposedCount);
+			}
+			Assert.AreEqual(1, DisposableFoo.DisposedCount);
+		}
+
+		[Test]
+		public void Scoped_component_is_not_released_by_call_to_container_Release()
+		{
+			DisposableFoo foo;
+			DisposableFoo.DisposedCount = 0;
+
+			Container.Register(Component.For<DisposableFoo>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				foo = Container.Resolve<DisposableFoo>();
+				Container.Release(foo);
+				Assert.AreEqual(0, DisposableFoo.DisposedCount);
+			}
+		}
+
+		[Test]
+		[Ignore("This fails... because it's implemented for the old model")]
+		public void Scoped_component_is_not_tracked_by_the_release_policy()
+		{
+			DisposableFoo foo;
+			DisposableFoo.DisposedCount = 0;
+
+			Container.Register(Component.For<DisposableFoo>().LifeStyle.Scoped());
+
+			using (Container.BeginScope())
+			{
+				foo = Container.Resolve<DisposableFoo>();
+				Assert.IsFalse(Kernel.ReleasePolicy.HasTrack(foo));
+			}
 		}
 	}
 }
