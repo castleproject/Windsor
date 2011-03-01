28c3173155749d77148d8c018a8ba73dbd4e9360
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-01 23:29:32 +1000

    - did some clean up and updated a test to expect different type of exception

--- src/Castle.Windsor.Tests/Bugs/IoC_141.cs
+++ src/Castle.Windsor.Tests/Bugs/IoC_141.cs
@@ -1,44 +1,27 @@
-using Castle.MicroKernel.Handlers;
-using NUnit.Framework;
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Castle.MicroKernel.Tests.Bugs
 {
-    using System;
+	using Castle.MicroKernel.Registration;
+	using Castle.MicroKernel.Resolvers;
 
-    using Castle.MicroKernel.Registration;
+	using NUnit.Framework;
 
-    [TestFixture]
+	[TestFixture]
 	public class IoC_141
 	{
-		[Test]
-		public void Can_resolve_open_generic_service_with_closed_generic_parameter()
-		{
-			var kernel = new DefaultKernel();
-			((IKernel)kernel).Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
-			((IKernel)kernel).Register(Component.For(typeof(IAssembler<object>)).ImplementedBy(typeof(ObjectAssembler)).Named("assembler"));
-			Assert.IsInstanceOf(typeof(DefaultProcessor<object>), kernel.Resolve<IProcessor<object>>());
-		}
-
-		[Test]
-		public void Can_resolve_service_with_open_generic_parameter_with_closed_generic_parameter()
-		{
-			var kernel = new DefaultKernel();
-			((IKernel)kernel).Register(Component.For(typeof(IService)).ImplementedBy(typeof(Service1)).Named("service1"));
-			((IKernel)kernel).Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
-			((IKernel)kernel).Register(Component.For(typeof(IAssembler<object>)).ImplementedBy(typeof(ObjectAssembler)).Named("assembler"));
-			Assert.IsInstanceOf(typeof(Service1), kernel.Resolve<IService>());
-		}
-
-		[Test]
-		[ExpectedException(typeof(HandlerException))]
-		public void Throws_right_exception_when_not_found_matching_generic_service()
-		{
-			var kernel = new DefaultKernel();
-			((IKernel)kernel).Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
-			((IKernel)kernel).Register(Component.For(typeof(IAssembler<object>)).ImplementedBy(typeof(ObjectAssembler)).Named("assembler"));
-			kernel.Resolve<IProcessor<int>>();
-		}
-
 		public interface IService
 		{
 		}
@@ -68,6 +51,34 @@ namespace Castle.MicroKernel.Tests.Bugs
 		public class ObjectAssembler : IAssembler<object>
 		{
 		}
-	}
 
+		[Test]
+		public void Can_resolve_open_generic_service_with_closed_generic_parameter()
+		{
+			var kernel = new DefaultKernel();
+			((IKernel)kernel).Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
+			((IKernel)kernel).Register(Component.For(typeof(IAssembler<object>)).ImplementedBy(typeof(ObjectAssembler)).Named("assembler"));
+			Assert.IsInstanceOf(typeof(DefaultProcessor<object>), kernel.Resolve<IProcessor<object>>());
+		}
+
+		[Test]
+		public void Can_resolve_service_with_open_generic_parameter_with_closed_generic_parameter()
+		{
+			var kernel = new DefaultKernel();
+			((IKernel)kernel).Register(Component.For(typeof(IService)).ImplementedBy(typeof(Service1)).Named("service1"));
+			((IKernel)kernel).Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
+			((IKernel)kernel).Register(Component.For(typeof(IAssembler<object>)).ImplementedBy(typeof(ObjectAssembler)).Named("assembler"));
+			Assert.IsInstanceOf(typeof(Service1), kernel.Resolve<IService>());
+		}
+
+		[Test]
+		[ExpectedException(typeof(DependencyResolverException))]
+		public void Throws_right_exception_when_not_found_matching_generic_service()
+		{
+			var kernel = new DefaultKernel();
+			kernel.Register(Component.For(typeof(IProcessor<>)).ImplementedBy(typeof(DefaultProcessor<>)).Named("processor"));
+			kernel.Register(Component.For<IAssembler<object>>().ImplementedBy<ObjectAssembler>().Named("assembler"));
+			kernel.Resolve<IProcessor<int>>();
+		}
+	}
 }
