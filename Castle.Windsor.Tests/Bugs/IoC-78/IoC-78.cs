5bda5dcf75ffd45fe51761ffe853d7d3270ae3af
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-01 20:10:47 +1000

    - clean up, and updated names of the tests

--- Castle.Windsor.Tests/Bugs/IoC-78/IoC-78.cs
+++ Castle.Windsor.Tests/Bugs/IoC-78/IoC-78.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,101 +12,80 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Castle.Core;
-using Castle.MicroKernel.Handlers;
-using NUnit.Framework;
-
 namespace Castle.Windsor.Tests.Bugs.IoC_78
 {
-    using Castle.MicroKernel.Registration;
+	using Castle.MicroKernel.Handlers;
+	using Castle.MicroKernel.Registration;
 
-    [TestFixture]
-    public class IoC78
-    {
-        [Test]
-        public void WillIgnoreComponentsThatAreAlreadyInTheDependencyTracker_Constructor()
-        {
-            IWindsorContainer container = new WindsorContainer();
-            container.Register(Component.For(typeof(IChain)).ImplementedBy(typeof(MyChain)).Named("chain"));
-            container.Register(Component.For(typeof(IChain)).ImplementedBy(typeof(MyChain2)).Named("chain2"));
+	using NUnit.Framework;
 
-            IChain resolve = container.Resolve<IChain>("chain2");
-            Assert.IsNotNull(resolve);
-        }
+	[TestFixture]
+	public class IoC78 : AbstractContainerTestCase
+	{
+		[Test]
+		public void Will_Ignore_Components_Already_in_Dependency_Tracker_Constructor()
+		{
+			Container.Register(Component.For<IChain>().ImplementedBy<MyChain>().Named("chain"));
+			Container.Register(Component.For<IChain>().ImplementedBy<MyChain2>().Named("chain2"));
 
+			var resolve = Container.Resolve<IChain>("chain2");
+			Assert.IsNotNull(resolve);
+		}
 
-        [Test]
-        public void WillIgnoreComponentsThatAreAlreadyInTheDependencyTracker_Property()
-        {
-            IWindsorContainer container = new WindsorContainer();
-            container.Register(Component.For(typeof(IChain)).ImplementedBy(typeof(MyChain3)).Named("chain"));
+		[Test]
+		public void Will_Ignore_Components_Already_in_Dependency_Tracker_Property()
+		{
+			Container.Register(Component.For<IChain>().ImplementedBy<MyChain3>());
 
-            IChain resolve = container.Resolve<IChain>("chain");
-            Assert.IsNotNull(resolve);
-        }
+			var chain3 = (MyChain3)Container.Resolve<IChain>();
+			Assert.IsNull(chain3.Chain);
+		}
 
-        [Test]
-        [ExpectedException(typeof(HandlerException))]
-        public void WillNotTryToResolveAComponentToItself()
-        {
-            IWindsorContainer container = new WindsorContainer();
-            container.Register(Component.For(typeof(IChain)).ImplementedBy(typeof(MyChain4)).Named("chain"));
+		[Test]
+		[ExpectedException(typeof(HandlerException))]
+		public void Will_Not_Try_To_Resolve_Component_To_Itself()
+		{
+			Container.Register(Component.For<IChain>().ImplementedBy<MyChain4>());
 
-            container.Resolve<IChain>("chain");
-        }
-    }
+			Container.Resolve<IChain>();
+		}
+	}
 
-    public interface IChain
-    {
+	public interface IChain
+	{
+	}
 
-    }
+	public class MyChain : IChain
+	{
+		public MyChain()
+		{
+		}
 
-    public class MyChain : IChain
-    {
-        public MyChain()
-        {
+		public MyChain(IChain chain)
+		{
+		}
+	}
 
-        }
+	public class MyChain2 : IChain
+	{
+		public MyChain2()
+		{
+		}
 
-        public MyChain(IChain chain)
-        {
+		public MyChain2(IChain chain)
+		{
+		}
+	}
 
-        }
-    }
+	public class MyChain4 : IChain
+	{
+		public MyChain4(IChain chain)
+		{
+		}
+	}
 
-    public class MyChain2 : IChain
-    {
-        public MyChain2()
-        {
-
-        }
-
-        public MyChain2(IChain chain)
-        {
-
-        }
-    }
-
-    public class MyChain4 : IChain
-    {
-        public MyChain4(IChain chain)
-        {
-
-        }
-    }
-
-
-    public class MyChain3 : IChain
-    {
-        private IChain chain;
-
-        public IChain Chain
-        {
-            get { return chain; }
-            set { chain = value; }
-        }
-    }
+	public class MyChain3 : IChain
+	{
+		public IChain Chain { get; set; }
+	}
 }
