f8cf948c16d6f1bab4d1ae0cfb50a317d9356ab8
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-04 14:26:57 +1000

    - cleaned up DependencyResolvingTestCase to make room for some new tests

--- Castle.Windsor.Tests/DependencyResolvingTestCase.cs
+++ Castle.Windsor.Tests/DependencyResolvingTestCase.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,63 +15,36 @@
 namespace Castle.MicroKernel.Tests
 {
 	using Castle.MicroKernel.Registration;
+	using Castle.Windsor.Tests;
+	using Castle.Windsor.Tests.Components;
 
 	using NUnit.Framework;
 
 	[TestFixture]
-	public class DependencyResolvingTestCase
+	public class DependencyResolvingTestCase : AbstractContainerTestCase
 	{
 		[Test]
 		public void ContainerShouldUseFirstRegisteredDependencyOfTypeByDefault_EmailRegisteredFirst()
 		{
-			IKernel kernel = new DefaultKernel();
-			kernel.Register(Component.For(typeof(IAlarmSender)).ImplementedBy(typeof(EmailSender)).Named("email"));
-			kernel.Register(Component.For(typeof(IAlarmSender)).ImplementedBy(typeof(SmsSender)).Named("sms"));
+			Kernel.Register(Component.For<IAlarmSender>().ImplementedBy<EmailSender>(),
+			                Component.For<IAlarmSender>().ImplementedBy<SmsSender>(),
+			                Component.For<AlarmGenerator>());
 
-			kernel.Register(Component.For(typeof(AlarmGenerator)).Named("generator"));
+			var gen = Kernel.Resolve<AlarmGenerator>();
 
-			var gen = kernel.Resolve<AlarmGenerator>("generator");
-			Assert.AreEqual(typeof(EmailSender), gen.Sender.GetType());
+			Assert.IsInstanceOf<EmailSender>(gen.Sender);
 		}
 
 		[Test]
 		public void ContainerShouldUseFirstRegisteredDependencyOfTypeByDefault_SmsRegisteredFirst()
 		{
-			IKernel kernel = new DefaultKernel();
-			kernel.Register(Component.For(typeof(IAlarmSender)).ImplementedBy(typeof(SmsSender)).Named("sms"));
-			kernel.Register(Component.For(typeof(IAlarmSender)).ImplementedBy(typeof(EmailSender)).Named("email"));
+			Kernel.Register(Component.For<IAlarmSender>().ImplementedBy<SmsSender>(),
+			                Component.For<IAlarmSender>().ImplementedBy<EmailSender>(),
+			                Component.For<AlarmGenerator>());
 
-			kernel.Register(Component.For(typeof(AlarmGenerator)).Named("generator"));
+			var gen = Kernel.Resolve<AlarmGenerator>();
 
-			var gen = kernel.Resolve<AlarmGenerator>("generator");
-			Assert.AreEqual(typeof(SmsSender), gen.Sender.GetType());
-		}
-	}
-
-	public interface IAlarmSender
-	{
-	}
-
-	public class EmailSender : IAlarmSender
-	{
-	}
-
-	public class SmsSender : IAlarmSender
-	{
-	}
-
-	public class AlarmGenerator
-	{
-		private readonly IAlarmSender sender;
-
-		public AlarmGenerator(IAlarmSender sender)
-		{
-			this.sender = sender;
-		}
-
-		public IAlarmSender Sender
-		{
-			get { return sender; }
+			Assert.IsInstanceOf<SmsSender>(gen.Sender);
 		}
 	}
 }
