d89ff82f5bc37d61601584f02e029011d42d60c7
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-05 11:14:38 +1000

    - a little clean up, and swapped a foreach for a parametrized test

--- Castle.Windsor.Tests/Configuration2/ConfigWithStatementsTestCase.cs
+++ Castle.Windsor.Tests/Configuration2/ConfigWithStatementsTestCase.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,19 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if !SILVERLIGHT // we do not support xml config on SL
+
+#if !SILVERLIGHT
+// we do not support xml config on SL
 
 namespace Castle.Windsor.Tests.Configuration2
 {
-	using Castle.Core.Configuration;
-	using Castle.MicroKernel.SubSystems.Configuration;
-
 	using NUnit.Framework;
 
 	[TestFixture]
 	public class ConfigWithStatementsTestCase
 	{
 		private IWindsorContainer container;
+
+		[TestCase("debug")]
+		[TestCase("prod")]
+		[TestCase("qa")]
+		[TestCase("default")]
+		public void SimpleChoose(string flag)
+		{
+			var file = ConfigHelper.ResolveConfigPath("Configuration2/config_with_define_{0}.xml", flag);
+
+			container = new WindsorContainer(file);
+
+			var store = container.Kernel.ConfigurationStore;
+
+			Assert.AreEqual(1, store.GetComponents().Length);
+
+			var config = store.GetComponentConfiguration(flag);
+
+			Assert.IsNotNull(config);
+		}
 
 		[Test]
 		public void SimpleIf()
@@ -53,25 +71,6 @@ namespace Castle.Windsor.Tests.Configuration2
 
 			config = store.GetComponentConfiguration("notprod");
 			Assert.IsNotNull(config);
-		}
-
-		[Test]
-		public void SimpleChoose()
-		{
-			foreach(string flag in new[] {"debug", "prod", "qa", "default"})
-			{
-				var file = ConfigHelper.ResolveConfigPath("Configuration2/config_with_define_{0}.xml", flag);
-
-				container = new WindsorContainer(file);
-
-				IConfigurationStore store = container.Kernel.ConfigurationStore;
-
-				Assert.AreEqual(1, store.GetComponents().Length);
-
-				IConfiguration config = store.GetComponentConfiguration(flag);
-
-				Assert.IsNotNull(config);
-			}
 		}
 	}
 }
