cfd13acb935486b17c7f10fc53e32a8d8827071b
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-09 14:13:38 +1000

    - moved IWindsorContainer and WindsorContainer obsolete members to separate file

--- Castle.Windsor/Windsor/WindsorContainer.cs
+++ Castle.Windsor/Windsor/WindsorContainer.cs
@@ -17,7 +17,6 @@ namespace Castle.Windsor
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
-	using System.ComponentModel;
 	using System.Diagnostics;
 
 	using Castle.Core;
@@ -41,7 +40,7 @@ namespace Castle.Windsor
 	public class WindsorContainer : IWindsorContainer
 #else
 	[DebuggerTypeProxy(typeof(KernelDebuggerProxy))]
-	public class WindsorContainer : MarshalByRefObject, IWindsorContainer
+	public partial class WindsorContainer : MarshalByRefObject, IWindsorContainer
 #endif
 	{
 		private readonly Dictionary<string, IWindsorContainer> childContainers = new Dictionary<string, IWindsorContainer>();
@@ -255,20 +254,6 @@ namespace Castle.Windsor
 			get { return installer; }
 		}
 
-		[Obsolete("Use Resolve(key, new Arguments()) or Resolve<TService>(key) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual object this[String key]
-		{
-			get { return Resolve<object>(key); }
-		}
-
-		[Obsolete("Use Resolve(service) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual object this[Type service]
-		{
-			get { return Resolve(service); }
-		}
-
 		/// <summary>
 		///   Returns the inner instance of the MicroKernel
 		/// </summary>
@@ -374,174 +359,6 @@ namespace Castle.Windsor
 			}
 		}
 
-		[Obsolete("Use Register(Component.For(classType).Named(key)) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual IWindsorContainer AddComponent(String key, Type classType)
-		{
-			kernel.AddComponent(key, classType);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For(serviceType).ImplementedBy(classType).Named(key)) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual IWindsorContainer AddComponent(String key, Type serviceType, Type classType)
-		{
-			kernel.AddComponent(key, serviceType, classType);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>()) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponent<T>()
-		{
-			var t = typeof(T);
-			AddComponent(t.FullName, t);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>().Named(key)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponent<T>(string key)
-		{
-			AddComponent(key, typeof(T));
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<I>().ImplementedBy<T>().Named(key)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponent<I, T>(string key) where T : class
-		{
-			AddComponent(key, typeof(I), typeof(T));
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<I>().ImplementedBy<T>()) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponent<I, T>() where T : class
-		{
-			var t = typeof(T);
-			AddComponent(t.FullName, typeof(I), t);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For(classType).Named(key).Lifestyle.Is(lifestyle)) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle(string key, Type classType, LifestyleType lifestyle)
-		{
-			kernel.AddComponent(key, classType, lifestyle, true);
-			return this;
-		}
-
-		[Obsolete(
-			"Use Register(Component.For(serviceType).ImplementedBy(classType).Named(key).Lifestyle.Is(lifestyle)) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle(string key, Type serviceType, Type classType, LifestyleType lifestyle)
-		{
-			kernel.AddComponent(key, serviceType, classType, lifestyle, true);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>().Lifestyle.Is(lifestyle)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle<T>(LifestyleType lifestyle)
-		{
-			var t = typeof(T);
-			AddComponentLifeStyle(t.FullName, t, lifestyle);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<I>().ImplementedBy<T>().Lifestyle.Is(lifestyle)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle<I, T>(LifestyleType lifestyle) where T : class
-		{
-			var t = typeof(T);
-			AddComponentLifeStyle(t.FullName, typeof(I), t, lifestyle);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>().Named(key).Lifestyle.Is(lifestyle)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle<T>(string key, LifestyleType lifestyle)
-		{
-			AddComponentLifeStyle(key, typeof(T), lifestyle);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<I>().ImplementedBy<T>().Named(key).Lifestyle.Is(lifestyle)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentLifeStyle<I, T>(string key, LifestyleType lifestyle) where T : class
-		{
-			AddComponentLifeStyle(key, typeof(I), typeof(T), lifestyle);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<I>().ImplementedBy<T>().ExtendedProperties(extendedProperties)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentProperties<I, T>(IDictionary extendedProperties) where T : class
-		{
-			var t = typeof(T);
-			AddComponentWithProperties(t.FullName, typeof(I), t, extendedProperties);
-			return this;
-		}
-
-		[Obsolete(
-			"Use Register(Component.For<I>().ImplementedBy<T>().Named(key).ExtendedProperties(extendedProperties)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentProperties<I, T>(string key, IDictionary extendedProperties) where T : class
-		{
-			AddComponentWithProperties(key, typeof(I), typeof(T), extendedProperties);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For(classType).Named(key).ExtendedProperties(extendedProperties)) or generic version instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual IWindsorContainer AddComponentWithProperties(string key, Type classType, IDictionary extendedProperties)
-		{
-			kernel.AddComponentWithExtendedProperties(key, classType, extendedProperties);
-			return this;
-		}
-
-		[Obsolete(
-			"Use Register(Component.For(serviceType).ImplementedBy(classType).Named(key).ExtendedProperties(extendedProperties)) or generic version instead."
-			)]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual IWindsorContainer AddComponentWithProperties(string key, Type serviceType, Type classType,
-		                                                            IDictionary extendedProperties)
-		{
-			kernel.AddComponentWithExtendedProperties(key, serviceType, classType, extendedProperties);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>().ExtendedProperties(extendedProperties)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentWithProperties<T>(IDictionary extendedProperties)
-		{
-			var t = typeof(T);
-			AddComponentWithProperties(t.FullName, t, extendedProperties);
-			return this;
-		}
-
-		[Obsolete("Use Register(Component.For<T>().Named(key).ExtendedProperties(extendedProperties)) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddComponentWithProperties<T>(string key, IDictionary extendedProperties)
-		{
-			AddComponentWithProperties(key, typeof(T), extendedProperties);
-			return this;
-		}
-
-		/// <summary>
-		///   Registers a facility within the container.
-		/// </summary>
-		/// <param name = "idInConfiguration"></param>
-		/// <param name = "facility"></param>
-		[Obsolete("Use AddFacility(IFacility) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual IWindsorContainer AddFacility(String idInConfiguration, IFacility facility)
-		{
-			kernel.AddFacility(idInConfiguration, facility);
-			return this;
-		}
-
 		/// <summary>
 		///   Registers a facility within the container.
 		/// </summary>
@@ -549,36 +366,6 @@ namespace Castle.Windsor
 		public IWindsorContainer AddFacility(IFacility facility)
 		{
 			kernel.AddFacility(facility);
-			return this;
-		}
-
-		/// <summary>
-		///   Creates and adds an <see cref = "IFacility" /> facility to the container.
-		/// </summary>
-		/// <typeparam name = "T">The facility type.</typeparam>
-		/// <param name = "idInConfiguration"></param>
-		/// <returns></returns>
-		[Obsolete("Use AddFacility<TFacility>() instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddFacility<T>(String idInConfiguration) where T : IFacility, new()
-		{
-			kernel.AddFacility<T>(idInConfiguration);
-			return this;
-		}
-
-		/// <summary>
-		///   Creates and adds an <see cref = "IFacility" /> facility to the container.
-		/// </summary>
-		/// <typeparam name = "T">The facility type.</typeparam>
-		/// <param name = "idInConfiguration"></param>
-		/// <param name = "configureFacility">The callback for creation.</param>
-		/// <returns></returns>
-		[Obsolete("Use AddFacility<TFacility>(Action<TFacility>) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public IWindsorContainer AddFacility<T>(String idInConfiguration, Action<T> configureFacility)
-			where T : IFacility, new()
-		{
-			kernel.AddFacility(idInConfiguration, configureFacility);
 			return this;
 		}
 
@@ -724,32 +511,6 @@ namespace Castle.Windsor
 		public virtual object Resolve(Type service, object argumentsAsAnonymousType)
 		{
 			return Resolve(service, new ReflectionBasedDictionaryAdapter(argumentsAsAnonymousType));
-		}
-
-		/// <summary>
-		///   Returns a component instance by the key
-		/// </summary>
-		/// <param name = "key"></param>
-		/// <param name = "arguments"></param>
-		/// <returns></returns>
-		[Obsolete("Use Resolve<object>(key, arguments) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual object Resolve(String key, IDictionary arguments)
-		{
-			return kernel.Resolve<object>(key, arguments);
-		}
-
-		/// <summary>
-		///   Returns a component instance by the key
-		/// </summary>
-		/// <param name = "key"></param>
-		/// <param name = "argumentsAsAnonymousType"></param>
-		/// <returns></returns>
-		[Obsolete("Use Resolve<object>(key, argumentsAsAnonymousType) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public virtual object Resolve(String key, object argumentsAsAnonymousType)
-		{
-			return Resolve<object>(key, new ReflectionBasedDictionaryAdapter(argumentsAsAnonymousType));
 		}
 
 		/// <summary>
