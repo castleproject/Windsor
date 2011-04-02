560e4beae6f1334b53b2410a8cc13d84fe07ed53
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-04-03 09:28:07 +1000

    - moving the remaining debugger diagnostics to use IDiagnostic<> implementation underneath starting with AllComponents

--- Castle.Windsor/Windsor/Experimental/Diagnostics/AllComponentsDiagnostic.cs
+++ Castle.Windsor/Windsor/Experimental/Diagnostics/AllComponentsDiagnostic.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,16 +14,20 @@
 
 namespace Castle.Windsor.Experimental.Diagnostics
 {
-	/// <summary>
-	/// Base inteface for exposing diagnostic information about container, its components and extensions
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public interface IDiagnostic<out T>
+	using Castle.MicroKernel;
+
+	public class AllComponentsDiagnostic : IAllComponentsDiagnostic
 	{
-		/// <summary>
-		/// Collects specified diagnostic information.
-		/// </summary>
-		/// <returns></returns>
-		T Inspect();
+		private readonly IKernel kernel;
+
+		public AllComponentsDiagnostic(IKernel kernel)
+		{
+			this.kernel = kernel;
+		}
+
+		public IHandler[] Inspect()
+		{
+			return kernel.GetAssignableHandlers(typeof(object));
+		}
 	}
 }
--- Castle.Windsor/Windsor/Experimental/Diagnostics/IAllComponentsDiagnostic.cs
+++ Castle.Windsor/Windsor/Experimental/Diagnostics/IAllComponentsDiagnostic.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,16 +14,12 @@
 
 namespace Castle.Windsor.Experimental.Diagnostics
 {
+	using Castle.MicroKernel;
+
 	/// <summary>
-	/// Base inteface for exposing diagnostic information about container, its components and extensions
+	///   Collects all handlers for components in hosting container.
 	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public interface IDiagnostic<out T>
+	public interface IAllComponentsDiagnostic : IDiagnostic<IHandler[]>
 	{
-		/// <summary>
-		/// Collects specified diagnostic information.
-		/// </summary>
-		/// <returns></returns>
-		T Inspect();
 	}
 }
