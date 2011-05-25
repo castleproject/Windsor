7a8f1e1d23e2a8a6986b778c1e606a4a96fc0efa
hconceicao <henry.conceicao@gmail.com>
2011-05-24 22:47:54 -0300

    fixed some copyright notices

--- Castle.Facilities.AutoTx.Tests/InitTests.cs
+++ Castle.Facilities.AutoTx.Tests/InitTests.cs
@@ -1,6 +1,6 @@
 #region license
 
-// Copyright 2009-2011 Henrik Feldt - http://logibit.se/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
