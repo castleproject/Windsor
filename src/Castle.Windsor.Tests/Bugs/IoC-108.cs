4d2d9f0dd6fb30633fae927b49986e23d1b6ee71
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-08-10 20:46:01 +1000

    - use tabs not spaces

--- src/Castle.Windsor.Tests/Bugs/IoC-108.cs
+++ src/Castle.Windsor.Tests/Bugs/IoC-108.cs
@@ -1,53 +1,54 @@
-using System;
-using NUnit.Framework;
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-namespace Castle.MicroKernel.Tests.Bugs
+namespace CastleTests.Bugs
 {
-    using Castle.MicroKernel.Registration;
+	using Castle.MicroKernel.Registration;
 
-    [TestFixture]
-	public class IoC_108
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class IoC_108 : AbstractContainerTestCase
 	{
-		[Test]
-		public void Should_not_fail_when_constructor_parameter_and_public_property_with_private_setter_have_same_name()
-		{
-			IKernel kernel = new DefaultKernel();
-
-		    kernel.Register(Component.For<Service2>());
-		    kernel.Register(Component.For<Service1>());
-
-		    try
-			{
-				Service2 svc = kernel.Resolve<Service2>();
-				Assert.IsNotNull(svc);
-			}
-			catch (NullReferenceException)
-			{
-				Assert.Fail("Should not have thrown a NullReferenceException");
-			}
-		}
-
 		public class Service1
 		{
-			public void OpA() { }
-			public void OpB() { }
+			public void OpA()
+			{
+			}
+
+			public void OpB()
+			{
+			}
 		}
 
 		public class Service2
 		{
-			private Service1 m_Service1;
-
-			public Service1 Service1
-			{
-				get { return m_Service1; }
-				private set { m_Service1 = value; }
-			}
-
 			public Service2(Service1 service1)
 			{
 				Service1 = service1;
 			}
+
+			public Service1 Service1 { get; private set; }
+		}
+
+		[Test]
+		public void Should_not_fail_when_constructor_parameter_and_public_property_with_private_setter_have_same_name()
+		{
+			Container.Register(Component.For<Service2>(),
+			                   Component.For<Service1>());
+
+			Container.Resolve<Service2>();
 		}
 	}
-
 }
--- src/Castle.Windsor.Tests/Bugs/IoC-115.cs
+++ src/Castle.Windsor.Tests/Bugs/IoC-115.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,92 +12,87 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Castle.Windsor;
-using NUnit.Framework;
-
 namespace Castle.Windsor.Tests.Bugs
 {
-    using System;
+	using Castle.MicroKernel.Registration;
 
-    using Castle.MicroKernel.Registration;
+	using NUnit.Framework;
 
-    [TestFixture]
-    public class IoC_115
-    {
-        [Test]
-        public void  Cannot_resolve_a_dependency_from_a_parent_container_under_certain_circumstances()
-        {
-            WindsorContainer parent = new WindsorContainer();
-            WindsorContainer child = new WindsorContainer();
+	[TestFixture]
+	public class IoC_115
+	{
+		public interface IParentService
+		{
+		}
 
-            parent.AddChildContainer(child);
+		public class ParentService : IParentService
+		{
+		}
 
-            ((IWindsorContainer)parent).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(ParentService)).Named("service"));
-            ((IWindsorContainer)child).Register(Component.For(typeof(IChildService1)).ImplementedBy(typeof(ChildService1)).Named("service1"));
-            ((IWindsorContainer)child).Register(Component.For(typeof(IChildService2)).ImplementedBy(typeof(ChildService2)).Named("service2"));
+		public interface IChildService1
+		{
+		}
 
+		public class ChildService1 : IChildService1
+		{
+			public ChildService1(IChildService2 xxx)
+			{
+			}
+		}
 
-            child.Resolve<IChildService1>();
-        }
+		public interface IChildService2
+		{
+			IParentService Parent { get; }
+		}
 
+		public class ChildService2 : IChildService2
+		{
+			private readonly IParentService xxx;
 
-        [Test]
-        public void Should_resolve_child_from_childs_container()
-        {
-            WindsorContainer parent = new WindsorContainer();
-            WindsorContainer child = new WindsorContainer();
+			public ChildService2(IParentService xxx)
+			{
+				this.xxx = xxx;
+			}
 
-            parent.AddChildContainer(child);
+			public IParentService Parent
+			{
+				get { return xxx; }
+			}
+		}
 
-            ((IWindsorContainer)parent).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(ParentService)).Named("service1"));
-            ((IWindsorContainer)parent).Register(Component.For(typeof(IChildService2)).ImplementedBy(typeof(ChildService2)).Named("service3"));
-            ((IWindsorContainer)child).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(AnotherParentService)).Named("service2"));
+		public class AnotherParentService : IParentService
+		{
+		}
 
-			IChildService2 resolve = child.Resolve<IChildService2>();
-            Assert.IsInstanceOf(typeof(AnotherParentService),resolve.Parent);
-        }
+		[Test]
+		public void Cannot_resolve_a_dependency_from_a_parent_container_under_certain_circumstances()
+		{
+			var parent = new WindsorContainer();
+			var child = new WindsorContainer();
 
-        public interface IParentService
-        {
-        }
+			parent.AddChildContainer(child);
 
-        public class ParentService : IParentService
-        {
-        }
+			((IWindsorContainer)parent).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(ParentService)).Named("service"));
+			((IWindsorContainer)child).Register(Component.For(typeof(IChildService1)).ImplementedBy(typeof(ChildService1)).Named("service1"));
+			((IWindsorContainer)child).Register(Component.For(typeof(IChildService2)).ImplementedBy(typeof(ChildService2)).Named("service2"));
 
-        public interface IChildService1
-        {
-        }
+			child.Resolve<IChildService1>();
+		}
 
-        public class ChildService1 : IChildService1
-        {
-            public ChildService1(IChildService2 xxx)
-            {
-            }
-        }
+		[Test]
+		public void Should_resolve_child_from_childs_container()
+		{
+			var parent = new WindsorContainer();
+			var child = new WindsorContainer();
 
-        public interface IChildService2
-        {
-            IParentService Parent { get; }
-        }
+			parent.AddChildContainer(child);
 
-        public class ChildService2 : IChildService2
-        {
-            private readonly IParentService xxx;
+			((IWindsorContainer)parent).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(ParentService)).Named("service1"));
+			((IWindsorContainer)parent).Register(Component.For(typeof(IChildService2)).ImplementedBy(typeof(ChildService2)).Named("service3"));
+			((IWindsorContainer)child).Register(Component.For(typeof(IParentService)).ImplementedBy(typeof(AnotherParentService)).Named("service2"));
 
-            public ChildService2(IParentService xxx)
-            {
-                this.xxx = xxx;
-            }
-
-            public IParentService Parent
-            {
-                get { return xxx; }
-            }
-        }
-
-        public class AnotherParentService : IParentService
-        {
-        }
-    }
+			var resolve = child.Resolve<IChildService2>();
+			Assert.IsInstanceOf(typeof(AnotherParentService), resolve.Parent);
+		}
+	}
 }
