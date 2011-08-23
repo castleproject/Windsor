84117b74f608c130dc94cced8d5d12857095d3ba
Henrik <henrik@haf.se>
2011-08-23 14:06:11 +0200

    moved new projs

---
+++ Castle.Transactions.IO/TransactionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Transactions;
+using Castle.IO;
+
+namespace Castle.Transactions.IO
+{
+	public static class TransactionExtensions
+	{
+		public static IFileSystem UpgradeToFileTransaction(this ITransaction tx)
+		{
+			return null;
+		}
+	}
+
+	public static class TransactionManagerExtensions
+	{
+		public static Maybe<ITransaction> CreateFileTransaction(this ITransactionManager manager,
+		                                                        ITransactionOptions options)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public sealed class FileTransactionAttribute : TransactionAttribute
+	{
+		public FileTransactionAttribute()
+		{
+		}
+
+		public FileTransactionAttribute(TransactionMode transactionMode) : base(transactionMode)
+		{
+		}
+
+		public FileTransactionAttribute(TransactionScopeOption mode) : base(mode)
+		{
+		}
+
+		public FileTransactionAttribute(TransactionScopeOption mode, IsolationLevel isolationLevel)
+			: base(mode, isolationLevel)
+		{
+		}
+	}
+}
