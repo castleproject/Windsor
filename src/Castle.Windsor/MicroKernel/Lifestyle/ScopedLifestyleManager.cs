17861264c43024a6d88d4059f021e62c9bde1433
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-11-26 15:35:30 +1000

    - throwing helpful exception if scope cannot be obtained in scoped lifestyle

--- src/Castle.Windsor/MicroKernel/Lifestyle/ScopedLifestyleManager.cs
+++ src/Castle.Windsor/MicroKernel/Lifestyle/ScopedLifestyleManager.cs
@@ -17,6 +17,8 @@ namespace Castle.MicroKernel.Lifestyle
 	using System;
 	using System.Threading;
 
+	using Castle.Core.Internal;
+
 	using Castle.MicroKernel.Context;
 	using Castle.MicroKernel.Lifestyle.Scoped;
 
@@ -45,12 +47,7 @@ namespace Castle.MicroKernel.Lifestyle
 
 		public override object Resolve(CreationContext context, IReleasePolicy releasePolicy)
 		{
-			var localScope = accessor;
-			if(localScope == null)
-			{
-				throw new ObjectDisposedException("Scope was already disposed. This is most likely a bug in the calling code.");
-			}
-			var scope = localScope.GetScope(context);
+			var scope = GetScope(context);
 			var burden = scope.GetCachedInstance(Model, afterCreated =>
 			{
 				var localBurden = base.CreateInstance(context, trackedExternally: true);
@@ -59,6 +56,26 @@ namespace Castle.MicroKernel.Lifestyle
 				return localBurden;
 			});
 			return burden.Instance;
+		}
+
+		private ILifetimeScope GetScope(CreationContext context)
+		{
+			var localScope = accessor;
+			if (localScope == null)
+			{
+				throw new ObjectDisposedException("Scope was already disposed. This is most likely a bug in the calling code.");
+			}
+			var scope = localScope.GetScope(context);
+			if(scope == null)
+			{
+				throw new ComponentResolutionException(
+					string.Format(
+						"Could not obtain scope for component {0}. This is most likely either a bug in custom {1} or you're trying to access scoped component outside of the scope (like a per-web-request component outside of web request etc)",
+						Model.Name,
+						typeof(IScopeAccessor).ToCSharpString()),
+					Model);
+			}
+			return scope;
 		}
 	}
 }
