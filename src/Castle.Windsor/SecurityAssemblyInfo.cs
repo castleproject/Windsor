20578bfb7c932419ced7b108b455101026f32c6c
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-07-01 14:16:07 +1000

    removed APTCA and security transparency in CP
    This was causing security exceptions all over the place in Client Profile due to security mismatch between Castle.Windsor.dll and Castle.Core.dll
    I'm not sure which way we want to proceed moving forward but for now I'd rather err on the side of safety

--- src/Castle.Windsor/SecurityAssemblyInfo.cs
+++ src/Castle.Windsor/SecurityAssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿// Sets up assembly level security settings
+#if ! CLIENTPROFILE // this could technically be removed if we decide to do something similar in Castle.Core.dll
 #if ! SILVERLIGHT
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #if !DOTNET35
 [assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level2)]
 #endif
 // [assembly: System.Security.SecurityTransparent]
+#endif
 #endif
