af34763a5fe5e40ce434f7529c486dc9c87950f4
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-07-01 11:36:05 +1000

    added failing test to reproduce IOC-345

--- src/Castle.Windsor.Tests/Facilities/TypedFactory/TypedFactoryAndSubContainersTestCase.cs
+++ src/Castle.Windsor.Tests/Facilities/TypedFactory/TypedFactoryAndSubContainersTestCase.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2012 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,35 +18,60 @@ namespace Castle.Windsor.Tests.Facilities.TypedFactory.Components
 	using Castle.MicroKernel.Registration;
 	using Castle.Windsor.Tests.Facilities.TypedFactory.Factories;
 
+	using CastleTests;
+	using CastleTests.Components;
 	using CastleTests.Facilities.TypedFactory.Factories;
 
 	using NUnit.Framework;
 
 	[TestFixture]
-	public class TypedFactoryAndSubContainersTestCase
+	public class TypedFactoryAndSubContainersTestCase : AbstractContainerTestCase
 	{
 		[Test]
 		public void Facility_When_added_to_a_child_container_wher_parent_has_facility_pulls_from_child()
 		{
-			var mainContainer = new WindsorContainer();
 			var childContainer = new WindsorContainer();
 
 			// NOTE: this has to happen in this order
-			mainContainer.AddChildContainer(childContainer);
-			mainContainer.AddFacility<TypedFactoryFacility>();
+			Container.AddChildContainer(childContainer);
+			Container.AddFacility<TypedFactoryFacility>();
 			childContainer.AddFacility<TypedFactoryFacility>();
 
-			mainContainer.Register(Component.For<IDummyComponent>().ImplementedBy<Component1>());
+			Container.Register(Component.For<IDummyComponent>().ImplementedBy<Component1>());
 			childContainer.Register(Component.For<IDummyComponentFactory>().AsFactory(),
 			                        Component.For<IDummyComponent>().ImplementedBy<Component2>());
 
-			var fromParent = mainContainer.Resolve<IDummyComponent>();
+			var fromParent = Container.Resolve<IDummyComponent>();
 			var fromFactory = childContainer.Resolve<IDummyComponentFactory>().CreateDummyComponent();
 			var fromChild = childContainer.Resolve<IDummyComponent>();
 
 			Assert.AreSame(fromFactory, fromChild);
 			Assert.AreNotSame(fromChild, fromParent);
 			Assert.AreNotSame(fromFactory, fromParent);
+		}
+
+		[Test]
+		[Bug("IOC-345")]
+		public void Resolve_SingletonAndDisposeChildContainer_ShouldNotDisposeSingleton()
+		{
+			Container.AddFacility<TypedFactoryFacility>();
+			Container.Register(Component.For<IGenericFactory<A>>().AsFactory(),
+			                   Component.For<A>());
+
+			// uncomment the line below and the test will not fail
+			//container.Resolve<ISomeFactory>();
+
+			var childContainer = new WindsorContainer();
+			Container.AddChildContainer(childContainer);
+
+			var factory = childContainer.Resolve<IGenericFactory<A>>();
+			Container.RemoveChildContainer(childContainer);
+			childContainer.Dispose();
+
+			factory = Container.Resolve<IGenericFactory<A>>();
+
+
+			factory.Create();
 		}
 	}
 }
