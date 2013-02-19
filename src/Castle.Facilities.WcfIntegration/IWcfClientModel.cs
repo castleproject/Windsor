ed7ff910d03db5bca53810b8121bc346e700f729
darkl <dark_leizar@walla.com>
2013-02-20 01:06:03 +0200

    Fixed a bit indentation

--- src/Castle.Facilities.WcfIntegration/IWcfClientModel.cs
+++ src/Castle.Facilities.WcfIntegration/IWcfClientModel.cs
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 // 
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	 http://www.apache.org/licenses/LICENSE-2.0
 // 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,13 +32,13 @@ namespace Castle.Facilities.WcfIntegration
 		/// </summary>
 		IWcfEndpoint Endpoint { get; }
 
-        /// <summary>
-        /// Gets a value indicating whether the proxy channel should be opened lazy,
-        /// or on activation.
-        /// </summary>
-        bool OpenChannelOnDemand { get; }
+		/// <summary>
+		/// Gets a value indicating whether the proxy channel should be opened lazy,
+		/// or on activation.
+		/// </summary>
+		bool OpenChannelOnDemand { get; }
 
-        /// <summary>
+		/// <summary>
 		/// Determines if async capability is desired.
 		/// </summary>
 		bool WantsAsyncCapability { get; }
