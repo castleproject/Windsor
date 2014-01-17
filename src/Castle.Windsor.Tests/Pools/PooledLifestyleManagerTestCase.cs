8bb87072b917619d008e286750568da6c28d159d
mvastarelli <michael.vastarelli@gmail.com>
2014-01-17 11:48:20 -0500

    Pooled items disposed properly when container is disposed.
    When a pool is disposed, all of the items that are being tracked by the
    pool are disposed properly.

--- src/Castle.Windsor.Tests/Pools/PooledLifestyleManagerTestCase.cs
+++ src/Castle.Windsor.Tests/Pools/PooledLifestyleManagerTestCase.cs
@@ -23,16 +23,18 @@ namespace Castle.MicroKernel.Tests.Pools
 	using CastleTests.Components;
 
 	using NUnit.Framework;
+using System;
+    using Castle.Windsor;
 
-	[TestFixture]
-	public class PooledLifestyleManagerTestCase : AbstractContainerTestCase
-	{
-		[Test]
-		public void MaxSize()
-		{
-			Kernel.Register(Component.For<PoolableComponent1>());
+    [TestFixture]
+    public class PooledLifestyleManagerTestCase : AbstractContainerTestCase
+    {
+        [Test]
+        public void MaxSize()
+        {
+            Kernel.Register(Component.For<PoolableComponent1>());
 
-			var instances = new List<PoolableComponent1>
+            var instances = new List<PoolableComponent1>
 			{
 				Kernel.Resolve<PoolableComponent1>(),
 				Kernel.Resolve<PoolableComponent1>(),
@@ -41,130 +43,167 @@ namespace Castle.MicroKernel.Tests.Pools
 				Kernel.Resolve<PoolableComponent1>()
 			};
 
-			var other1 = Kernel.Resolve<PoolableComponent1>();
+            var other1 = Kernel.Resolve<PoolableComponent1>();
 
-			CollectionAssert.DoesNotContain(instances, other1);
+            CollectionAssert.DoesNotContain(instances, other1);
 
-			foreach (var inst in instances)
-			{
-				Kernel.ReleaseComponent(inst);
-			}
+            foreach (var inst in instances)
+            {
+                Kernel.ReleaseComponent(inst);
+            }
 
-			Kernel.ReleaseComponent(other1);
+            Kernel.ReleaseComponent(other1);
 
-			var other2 = Kernel.Resolve<PoolableComponent1>();
+            var other2 = Kernel.Resolve<PoolableComponent1>();
 
-			Assert.AreNotEqual(other1, other2);
-			CollectionAssert.Contains(instances, other2);
+            Assert.AreNotEqual(other1, other2);
+            CollectionAssert.Contains(instances, other2);
 
-			Kernel.ReleaseComponent(other2);
-		}
+            Kernel.ReleaseComponent(other2);
+        }
 
-		[Test]
-		public void Poolable_component_is_always_tracked()
-		{
-			Kernel.Register(Component.For<A>().LifeStyle.Pooled);
+        [Test]
+        public void Poolable_component_is_always_tracked()
+        {
+            Kernel.Register(Component.For<A>().LifeStyle.Pooled);
 
-			var component = Kernel.Resolve<A>();
+            var component = Kernel.Resolve<A>();
 
-			Assert.IsTrue(Kernel.ReleasePolicy.HasTrack(component));
-		}
+            Assert.IsTrue(Kernel.ReleasePolicy.HasTrack(component));
+        }
 
-		[Test]
-		public void Recyclable_component_as_dependency_can_be_reused()
-		{
-			Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null),
-			                Component.For<UseRecyclableComponent>().LifeStyle.Transient);
-			var component = Kernel.Resolve<UseRecyclableComponent>();
-			Container.Release(component);
-			var componentAgain = Kernel.Resolve<UseRecyclableComponent>();
+        [Test]
+        public void Recyclable_component_as_dependency_can_be_reused()
+        {
+            Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null),
+                            Component.For<UseRecyclableComponent>().LifeStyle.Transient);
+            var component = Kernel.Resolve<UseRecyclableComponent>();
+            Container.Release(component);
+            var componentAgain = Kernel.Resolve<UseRecyclableComponent>();
 
-			Assert.AreSame(componentAgain.Dependency, component.Dependency);
+            Assert.AreSame(componentAgain.Dependency, component.Dependency);
 
-			Container.Release(componentAgain);
-			Assert.AreEqual(2, componentAgain.Dependency.RecycledCount);
-		}
+            Container.Release(componentAgain);
+            Assert.AreEqual(2, componentAgain.Dependency.RecycledCount);
+        }
 
-		[Test]
-		public void Recyclable_component_can_be_reused()
-		{
-			Kernel.Register(Component.For<RecyclableComponent>().LifestylePooled(1));
-			var component = Kernel.Resolve<RecyclableComponent>();
-			Container.Release(component);
-			var componentAgain = Kernel.Resolve<RecyclableComponent>();
+        [Test]
+        public void Recyclable_component_can_be_reused()
+        {
+            Kernel.Register(Component.For<RecyclableComponent>().LifestylePooled(1));
+            var component = Kernel.Resolve<RecyclableComponent>();
+            Container.Release(component);
+            var componentAgain = Kernel.Resolve<RecyclableComponent>();
 
-			Assert.AreSame(componentAgain, component);
+            Assert.AreSame(componentAgain, component);
 
-			Container.Release(componentAgain);
-			Assert.AreEqual(2, componentAgain.RecycledCount);
-		}
+            Container.Release(componentAgain);
+            Assert.AreEqual(2, componentAgain.RecycledCount);
+        }
 
-		[Test]
-		public void Recyclable_component_gets_recycled_just_once_on_subsequent_release()
-		{
-			Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null));
-			var component = Kernel.Resolve<RecyclableComponent>();
+        [Test]
+        public void Recyclable_component_gets_recycled_just_once_on_subsequent_release()
+        {
+            Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null));
+            var component = Kernel.Resolve<RecyclableComponent>();
 
-			Assert.AreEqual(0, component.RecycledCount);
+            Assert.AreEqual(0, component.RecycledCount);
 
-			Container.Release(component);
-			Container.Release(component);
-			Container.Release(component);
-			Assert.AreEqual(1, component.RecycledCount);
-		}
+            Container.Release(component);
+            Container.Release(component);
+            Container.Release(component);
+            Assert.AreEqual(1, component.RecycledCount);
+        }
 
-		[Test]
-		public void Recyclable_component_gets_recycled_on_release()
-		{
-			Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null));
-			var component = Kernel.Resolve<RecyclableComponent>();
+        [Test]
+        public void Recyclable_component_gets_recycled_on_release()
+        {
+            Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null));
+            var component = Kernel.Resolve<RecyclableComponent>();
 
-			Assert.AreEqual(0, component.RecycledCount);
+            Assert.AreEqual(0, component.RecycledCount);
 
-			Container.Release(component);
-			Assert.AreEqual(1, component.RecycledCount);
-		}
+            Container.Release(component);
+            Assert.AreEqual(1, component.RecycledCount);
+        }
 
-		[Test]
-		public void Recyclable_component_with_on_release_action_not_released_more_than_necessary()
-		{
-			var count = 0;
-			Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null)
-			                	.DynamicParameters((k, d) =>
-			                	{
-			                		count++;
-			                		return delegate { count--; };
-			                	}));
+        [Test]
+        public void Recyclable_component_with_on_release_action_not_released_more_than_necessary()
+        {
+            var count = 0;
+            Kernel.Register(Component.For<RecyclableComponent>().LifeStyle.PooledWithSize(1, null)
+                                .DynamicParameters((k, d) =>
+                                {
+                                    count++;
+                                    return delegate { count--; };
+                                }));
 
-			RecyclableComponent component = null;
-			for (var i = 0; i < 10; i++)
-			{
-				component = Kernel.Resolve<RecyclableComponent>();
-				Container.Release(component);
-			}
-			Assert.AreEqual(10, component.RecycledCount);
-			Assert.AreEqual(0, count);
-		}
+            RecyclableComponent component = null;
+            for (var i = 0; i < 10; i++)
+            {
+                component = Kernel.Resolve<RecyclableComponent>();
+                Container.Release(component);
+            }
+            Assert.AreEqual(10, component.RecycledCount);
+            Assert.AreEqual(0, count);
+        }
 
-		[Test]
-		public void SimpleUsage()
-		{
-			Kernel.Register(Component.For<PoolableComponent1>());
+        [Test]
+        public void SimpleUsage()
+        {
+            Kernel.Register(Component.For<PoolableComponent1>());
 
-			var inst1 = Kernel.Resolve<PoolableComponent1>();
-			var inst2 = Kernel.Resolve<PoolableComponent1>();
+            var inst1 = Kernel.Resolve<PoolableComponent1>();
+            var inst2 = Kernel.Resolve<PoolableComponent1>();
 
-			Kernel.ReleaseComponent(inst2);
-			Kernel.ReleaseComponent(inst1);
+            Kernel.ReleaseComponent(inst2);
+            Kernel.ReleaseComponent(inst1);
 
-			var other1 = Kernel.Resolve<PoolableComponent1>();
-			var other2 = Kernel.Resolve<PoolableComponent1>();
+            var other1 = Kernel.Resolve<PoolableComponent1>();
+            var other2 = Kernel.Resolve<PoolableComponent1>();
 
-			Assert.AreSame(inst1, other1);
-			Assert.AreSame(inst2, other2);
+            Assert.AreSame(inst1, other1);
+            Assert.AreSame(inst2, other2);
 
-			Kernel.ReleaseComponent(inst2);
-			Kernel.ReleaseComponent(inst1);
-		}
-	}
+            Kernel.ReleaseComponent(inst2);
+            Kernel.ReleaseComponent(inst1);
+        }
+
+        [Test]
+        public void DisposePoolDisposesTrackedComponents()
+        {
+            // Arrange.
+            bool result = false;
+            var container = new WindsorContainer();
+            container.Register(Component.For<DisposableMockObject>().LifestylePooled(1, 5));
+            container.Release(container.Resolve<DisposableMockObject>(new
+            {
+                disposeAction = new Action(() =>
+                    {
+                        result = true;
+                    })
+            }));
+
+            // Act.
+            container.Dispose();
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        public class DisposableMockObject : IDisposable
+        {
+            private Action disposeAction;
+
+            public DisposableMockObject(Action disposeAction)
+            {
+                this.disposeAction = disposeAction;
+            }
+            public void Dispose()
+            {
+                disposeAction();
+            }
+        }
+
+    }
 }
