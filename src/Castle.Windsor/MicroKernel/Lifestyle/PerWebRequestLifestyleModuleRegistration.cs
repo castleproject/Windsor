86d397c59fe467120afb45b8526b2a44f5c6f4d1
Mathieu Lafontaine <mathieulafontaine@hotmail.com>
2014-02-24 11:17:48 -0500

    Added support for module registration using HttpApplication.RegisterModule in .net 4.5
    Fix for issue #45 (https://github.com/castleproject/Windsor/issues/45)

--- src/Castle.Windsor/MicroKernel/Lifestyle/PerWebRequestLifestyleModuleRegistration.cs
+++ src/Castle.Windsor/MicroKernel/Lifestyle/PerWebRequestLifestyleModuleRegistration.cs
@@ -21,6 +21,7 @@ namespace Castle.MicroKernel.Lifestyle
 	using System.ComponentModel;
 	using System.Reflection;
 	using System.Security;
+    using System.Web;
 
 	[SecurityCritical]
 	[EditorBrowsable(EditorBrowsableState.Never)]
@@ -31,7 +32,10 @@ namespace Castle.MicroKernel.Lifestyle
 
 		public static void Run()
 		{
-			var dynamicModuleUtil = Type.GetType("Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility, " + MicrosoftWebInfrastructureDll,
+#if DOTNET45            
+            HttpApplication.RegisterModule(typeof(PerWebRequestLifestyleModule));            
+#else
+            var dynamicModuleUtil = Type.GetType("Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility, " + MicrosoftWebInfrastructureDll,
 			                                     throwOnError: false);
 			if (dynamicModuleUtil == null)
 			{
@@ -43,7 +47,8 @@ namespace Castle.MicroKernel.Lifestyle
 				return;
 			}
 			registerModule.Invoke(null, new object[] { typeof(PerWebRequestLifestyleModule) });
-		}
+#endif
+        }
 	}
 }
 
