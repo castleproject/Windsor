54900cb914ee05b722968d2036df85fdde293c2f
DaleFrancis88 <dalefrancis88@gmail.com>
2014-03-18 00:35:15 -0700

    Fix for Issue #50
    When someone uses the line
    Container.Register(Classes.FromThisAssembly().Pick().WithServiceFirstInterface());
    it seems to create an instance that causes
    ComponentConverter.CanHangleType to return true.
    
    If i switch the order of AttributeAwareConverter to be before
    ComponentConverter it works fine

---
+++ src/Castle.Windsor.Tests/Config/Components/ClassWithConfigDependency.cs
@@ -0,0 +1,76 @@
+﻿namespace Castle.MicroKernel.Tests.Configuration.Components
+{
+    using System.Linq;
+
+    /// <summary>
+    /// The ClassWithConfigDependency interface.
+    /// </summary>
+    public interface IClassWithConfigDependency
+    {
+        /// <summary>
+        /// The get name.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        string GetName();
+
+        /// <summary>
+        /// The get server ip.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        string GetServerIp(string name);
+    }
+
+    /// <summary>
+    /// The class with config dependency.
+    /// </summary>
+    public class ClassWithConfigDependency : IClassWithConfigDependency
+    {
+        /// <summary>
+        /// The _config.
+        /// </summary>
+        private readonly IConfig _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassWithConfigDependency"/> class.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        public ClassWithConfigDependency(IConfig config)
+        {
+            this._config = config;
+        }
+
+        /// <summary>
+        /// The get name.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string GetName()
+        {
+            return this._config.Name;
+        }
+
+        /// <summary>
+        /// The get server ip.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string GetServerIp(string name)
+        {
+            return this._config.Servers.First(s => s.Name == name).Ip;
+        }
+    }
+}
---
+++ src/Castle.Windsor.Tests/Config/Components/Config.cs
@@ -0,0 +1,53 @@
+﻿namespace Castle.MicroKernel.Tests.Configuration.Components
+{
+    using Castle.MicroKernel.SubSystems.Conversion;
+
+    /// <summary>
+    /// The Config interface.
+    /// </summary>
+    public interface IConfig
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the servers.
+        /// </summary>
+        Server[] Servers { get; set; }
+    }
+
+    /// <summary>
+    /// The config.
+    /// </summary>
+    public class Config : IConfig
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the servers.
+        /// </summary>
+        public Server[] Servers { get; set; }
+    }
+
+    /// <summary>
+    /// The server.
+    /// </summary>
+    [Convertible]
+    public class Server
+    {
+        /// <summary>
+        /// Gets or sets the ip.
+        /// </summary>
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
