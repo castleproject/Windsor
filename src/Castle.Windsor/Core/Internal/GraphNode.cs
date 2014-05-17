851ca166d072c1f266ced131d28c0df9faaf8b50
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2014-05-18 08:31:12 +1000

    fixing potential race condition under certain scenarios

--- src/Castle.Windsor/Core/Internal/GraphNode.cs
+++ src/Castle.Windsor/Core/Internal/GraphNode.cs
@@ -1,11 +1,8 @@
-// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
-// 
+// Copyright 2004-2014 Castle Project - http://www.castleproject.org/
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +12,7 @@
 namespace Castle.Core.Internal
 {
 	using System;
-	using System.Collections.Generic;
+	using System.Threading;
 
 	[Serializable]
 	public class GraphNode :
@@ -24,30 +21,30 @@ namespace Castle.Core.Internal
 #endif
 		IVertex
 	{
-
-		private List<GraphNode> outgoing;
+		private SimpleThreadSafeCollection<GraphNode> outgoing;
 
 		public void AddDependent(GraphNode node)
 		{
-			if (outgoing == null)
+			var collection = outgoing;
+			if (collection == null)
 			{
-				outgoing = new List<GraphNode>();
+				var @new = new SimpleThreadSafeCollection<GraphNode>();
+				collection = Interlocked.CompareExchange(ref outgoing, @new, null) ?? @new;
 			}
-			outgoing.Add(node);
+			collection.Add(node);
 		}
 
-		/// <summary>
-		///   The nodes that this node depends on
-		/// </summary>
+		/// <summary>The nodes that this node depends on</summary>
 		public GraphNode[] Dependents
 		{
 			get
 			{
-				if (outgoing == null)
+				var collection = outgoing;
+				if (collection == null)
 				{
 					return new GraphNode[0];
 				}
-				return outgoing.ToArray();
+				return collection.ToArray();
 			}
 		}
 
---
+++ src/Castle.Windsor/Core/Internal/SimpleThreadSafeCollection.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2004-2014 Castle Project - http://www.castleproject.org/
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Core.Internal
+{
+	using System.Collections.Generic;
+
+	public class SimpleThreadSafeCollection<T>
+	{
+		private readonly List<T> implementation = new List<T>();
+		private readonly Lock @lock = Lock.Create();
+
+		public int Count
+		{
+			get
+			{
+				using (@lock.ForReading())
+				{
+					return implementation.Count;
+				}
+			}
+		}
+
+		public void Add(T item)
+		{
+			using (@lock.ForWriting())
+			{
+				implementation.Add(item);
+			}
+		}
+
+		public bool Remove(T item)
+		{
+			using (@lock.ForWriting())
+			{
+				return implementation.Remove(item);
+			}
+		}
+
+		public T[] ToArray()
+		{
+			using (@lock.ForReading())
+			{
+				return implementation.ToArray();
+			}
+		}
+	}
+}
