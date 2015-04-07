e4caa95f7ae32a1f6db678cb2be71092622a0209
Dale Francis <dalefrancis88@gmail.com>
2015-04-06 18:08:23 -0700

    Issue 75 Fix
    Updated code to not use dictionary. It now looks through interfaces for
    the expected service type.
    
    I think this may have very minor performance difference but not sure
    there is enough that it matters

---
+++ src/Castle.Windsor.Tests/Generics/Component.cs
@@ -0,0 +1,30 @@
+﻿namespace CastleTests.Generics
+{
+    public interface ISingleton
+    {
+    }
+
+    public interface IOuterService<T1, T2> : ISingleton
+    {
+    }
+
+    public interface IWrap1<T> : ISingleton
+    {
+    }
+
+    public interface IWrap2<T> : ISingleton
+    {
+    }
+
+    public interface IService1<T1, T2> : IOuterService<IWrap1<T1>, T2>
+    {
+    }
+
+    public interface IService2<T1, T2> : IOuterService<IWrap2<T1>, T2>
+    {
+    }
+
+    public class Component<T1, T2> : IService1<T1, T2>, IService2<T1, T2>
+    {
+    }
+}
---
+++ src/Castle.Windsor.Tests/Issue75TestCase.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace CastleTests
+{
+    using Castle.MicroKernel.Registration;
+
+    using CastleTests.Generics;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Issue75TestCase : AbstractContainerTestCase
+    {
+        [Test]
+        public void SemiOpen_Generics_Still_Resolve()
+        {
+            this.Container.Register(
+            Classes
+                .FromAssemblyContaining<ISingleton>()
+                .BasedOn<ISingleton>()
+                .WithServiceFromInterface(typeof(ISingleton))
+                .WithServiceSelf());
+
+            var component = this.Container.Resolve<Component<int, int>>();
+
+            Assert.IsInstanceOf<ISingleton>(component);
+            Assert.IsInstanceOf<IOuterService<IWrap1<int>, int>>(component);
+            Assert.IsInstanceOf<IOuterService<IWrap2<int>, int>>(component);
+            Assert.IsInstanceOf<IService1<int, int>>(component);
+            Assert.IsInstanceOf<IService2<int, int>>(component);
+        }
+    }
+}
