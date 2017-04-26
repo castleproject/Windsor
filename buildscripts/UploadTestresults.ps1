963a8b53a9301b9244ccc657a0f0f22d6f2d03ce
Alina Popa <alpopa@microsoft.com>
2017-04-26 12:25:57 -0700

    Move powershell lines into AppVeyor.yml

--- buildscripts/UploadTestresults.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-# upload results to AppVeyor
-$wc = New-Object 'System.Net.WebClient'
-
-$testBinPath = "bin\Release\net45"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path "src\Castle.Windsor.Tests\$testBinPath\TestResult.xml"))
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path "src\Castle.Facilities.WcfIntegration.Tests\$testBinPath\TestResult.xml"))
-
