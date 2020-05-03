0c94508d7d7ddd488d8a749b139eb391fb77ac47
Lukas Tines <ltines@euroffice.co.uk>
2020-05-03 10:42:38 +0100

    Add comment why we're using MS namespace

--- src/Castle.Windsor.Extensions.MsDependencyInjection/HostBuilderExtensions.cs
+++ src/Castle.Windsor.Extensions.MsDependencyInjection/HostBuilderExtensions.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//Microsoft namespace is intentional - suggested by Microsoft
 namespace Microsoft.Extensions.Hosting
 {
     using Castle.Windsor;
