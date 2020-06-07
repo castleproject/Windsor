5699f6113f6d1d456ced13d03db6f55e9d3be355
Lukas Tines <ltines@euroffice.co.uk>
2020-06-07 10:27:17 +0100

    Fix intendation and remove debug test

--- src/Castle.Windsor.Extensions.DependencyInjection.Tests/WindsorScopedServiceProviderTests.cs
+++ src/Castle.Windsor.Extensions.DependencyInjection.Tests/WindsorScopedServiceProviderTests.cs
@@ -1,50 +1,17 @@
 namespace Castle.Windsor.Extensions.DependencyInjection.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Specification;
-    using Microsoft.Extensions.DependencyInjection.Specification.Fakes;
-    using Xunit;
+	using System;
 
-    public class WindsorScopedServiceProviderTests : DependencyInjectionSpecificationTests
-    {
-        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
-        {
-            var factory = new WindsorServiceProviderFactory();
-            var container = factory.CreateBuilder(serviceCollection);
-            return factory.CreateServiceProvider(container);
-        }
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.DependencyInjection.Specification;
 
-		[Fact]
-        public void RegistrationOrderIsPreservedWhenServicesAreIEnumerableResolved2()
-        {
-            // Arrange
-            var collection = new TestServiceCollection();
-            collection.AddTransient(typeof(IFakeMultipleService), typeof(FakeOneMultipleService));
-            collection.AddTransient(typeof(IFakeMultipleService), typeof(FakeTwoMultipleService));
-
-            var provider = CreateServiceProvider(collection);
-
-            collection.Reverse();
-            var providerReversed = CreateServiceProvider(collection);
-
-            // Act
-            var services = provider.GetService<IEnumerable<IFakeMultipleService>>();
-            var servicesReversed = providerReversed.GetService<IEnumerable<IFakeMultipleService>>();
-
-            // Assert
-            Assert.Collection(services,
-                service => Assert.IsType<FakeOneMultipleService>(service),
-                service => Assert.IsType<FakeTwoMultipleService>(service));
-
-            Assert.Collection(servicesReversed,
-                service => Assert.IsType<FakeTwoMultipleService>(service),
-                service => Assert.IsType<FakeOneMultipleService>(service));
-        }
-    }
-
-    internal class TestServiceCollection : List<ServiceDescriptor>, IServiceCollection
-    {
-    }
+	public class WindsorScopedServiceProviderTests : DependencyInjectionSpecificationTests
+	{
+		protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+		{
+			var factory = new WindsorServiceProviderFactory();
+			var container = factory.CreateBuilder(serviceCollection);
+			return factory.CreateServiceProvider(container);
+		}
+	}
 }
