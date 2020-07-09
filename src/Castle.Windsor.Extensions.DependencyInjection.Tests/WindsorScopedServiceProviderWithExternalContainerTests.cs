6b7980a6938f43edef0c1c23a8d75c9affa5fc0b
Rik Svendsen Rose <rsr@gait.dk>
2020-07-09 19:29:27 +0200

    Add WindsorScopedServiceProviderWithExternalContainerTests to ensure factory with extenal container passes
    #535

---
+++ src/Castle.Windsor.Extensions.DependencyInjection.Tests/WindsorScopedServiceProviderWithExternalContainerTests.cs
@@ -0,0 +1,32 @@
+// Copyright 2004-2020 Castle Project - http://www.castleproject.org/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Windsor.Extensions.DependencyInjection.Tests
+{
+	using System;
+
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.DependencyInjection.Specification;
+
+	public class WindsorScopedServiceProviderWithExternalContainerTests : DependencyInjectionSpecificationTests
+	{
+		protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+		{
+			var factory = new WindsorServiceProviderFactory(new WindsorContainer());
+			var container = factory.CreateBuilder(serviceCollection);
+			return factory.CreateServiceProvider(container);
+		}
+
+	}
+}
