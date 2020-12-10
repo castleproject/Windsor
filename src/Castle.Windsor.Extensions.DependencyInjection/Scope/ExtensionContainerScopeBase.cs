6d4d75ef8964ae62fd1e76cd76c0bfa2ac43627b
Rik Svendsen Rose <rsr@gait.dk>
2020-12-10 14:38:53 +0100

    Cleanup
    #563

--- src/Castle.Windsor.Extensions.DependencyInjection/Scope/ExtensionContainerScopeBase.cs
+++ src/Castle.Windsor.Extensions.DependencyInjection/Scope/ExtensionContainerScopeBase.cs
@@ -1,3 +1,17 @@
+// Copyright 2004-2020 Castle Project - http://www.castleproject.org/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 namespace Castle.Windsor.Extensions.DependencyInjection.Scope
 {
 	using System;
@@ -6,7 +20,7 @@ namespace Castle.Windsor.Extensions.DependencyInjection.Scope
 	using Castle.MicroKernel;
 	using Castle.MicroKernel.Lifestyle.Scoped;
 
-	internal  abstract class ExtensionContainerScopeBase : ILifetimeScope, IDisposable
+	internal  abstract class ExtensionContainerScopeBase : ILifetimeScope
 	{
 		public static string TransientMarker = "Transient";
 		private readonly IScopeCache scopeCache;
@@ -33,7 +47,7 @@ namespace Castle.Windsor.Extensions.DependencyInjection.Scope
 				// Add transient's burden to scope so it gets released
 				if (model.Configuration.Attributes.Get(TransientMarker) == bool.TrueString)
 				{
-					var transientBurden = createInstance((_) => {});
+					var transientBurden = createInstance(_ => {});
 					scopeCache[transientBurden] = transientBurden;
 					return transientBurden;
 				}
