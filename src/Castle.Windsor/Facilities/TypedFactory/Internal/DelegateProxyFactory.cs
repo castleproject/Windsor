f0ad3e198441179f3511699194a593e9d1d706b0
Jevon <jevon.farr@drdoctor.co.uk>
2022-05-14 10:09:12 +0100

    Update copyright years for files that have been changed as part of #612
    #612 - Updating Windsor to support Castle.Core@5.0.0 and modern TFMs

--- src/Castle.Windsor/Facilities/TypedFactory/Internal/DelegateProxyFactory.cs
+++ src/Castle.Windsor/Facilities/TypedFactory/Internal/DelegateProxyFactory.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2022 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,12 +15,10 @@
 namespace Castle.Facilities.TypedFactory.Internal
 {
 	using System;
-	using System.Reflection;
 
 	using Castle.Core;
 	using Castle.Core.Internal;
 	using Castle.DynamicProxy;
-	using Castle.DynamicProxy.Generators;
 	using Castle.MicroKernel;
 	using Castle.MicroKernel.Context;
 
