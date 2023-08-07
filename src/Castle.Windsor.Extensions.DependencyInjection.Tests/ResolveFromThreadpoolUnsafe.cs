4b3e30b3359d1b215a4f826004bc5fdb1e52d21f
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2023-08-07 13:41:53 +0200

    Added More failing test cases

--- src/Castle.Windsor.Extensions.DependencyInjection.Tests/ResolveFromThreadpoolUnsafe.cs
+++ src/Castle.Windsor.Extensions.DependencyInjection.Tests/ResolveFromThreadpoolUnsafe.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor.Extensions.DependencyInjection.Extensions;
 using Castle.Windsor.Extensions.DependencyInjection.Tests.Components;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -38,6 +39,84 @@ namespace Castle.Windsor.Extensions.DependencyInjection.Tests {
 			ThreadPool.UnsafeQueueUserWorkItem(state => {
 				try {
 					var actualUserService = sp.GetService<IUserService>();
+					Assert.NotNull(actualUserService);
+				}
+				catch (Exception ex) {
+					tcs.SetException(ex);
+					return;
+				}
+				tcs.SetResult(actualUserService);
+			}, null);
+
+			// Wait for the work item to complete.
+			var task = tcs.Task;
+			IUserService result = await task;
+			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public async Task Can_Resolve_From_CastleWindsor() {
+
+			var serviceProvider = new ServiceCollection();
+			var container = new WindsorContainer();
+			var f = new WindsorServiceProviderFactory(container);
+			f.CreateBuilder(serviceProvider);
+
+			container.Register(
+				// Component.For<IUserService>().ImplementedBy<UserService>().LifestyleNetTransient(),
+				Classes.FromThisAssembly().BasedOn<IUserService>().WithServiceAllInterfaces().LifestyleNetStatic()
+				);
+
+			IUserService actualUserService;
+			actualUserService = container.Resolve<IUserService>();
+			Assert.NotNull(actualUserService);
+
+			TaskCompletionSource<IUserService> tcs = new TaskCompletionSource<IUserService>();
+
+			ThreadPool.UnsafeQueueUserWorkItem(state => {
+				IUserService actualUserService = null;
+				try {
+					// resolving (with the underlying Castle Windsor, not using Service Provider) with a lifecycle that has an
+					// accessor that uses something that is AsyncLocal might be troublesome.
+					// the custom lifecycle accessor will kicks in, but noone assigns the Current scope (which is uninitialized)
+					actualUserService = container.Resolve<IUserService>();
+					Assert.NotNull(actualUserService);
+				}
+				catch (Exception ex) {
+					tcs.SetException(ex);
+					return;
+				}
+				tcs.SetResult(actualUserService);
+			}, null);
+
+			// Wait for the work item to complete.
+			var task = tcs.Task;
+			IUserService result = await task;
+			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public async Task Can_Resolve_From_ServiceProvider_cretaed_in_UnsafeQueueUserWorkItem() {
+
+			var serviceProvider = new ServiceCollection();
+			var container = new WindsorContainer();
+			var f = new WindsorServiceProviderFactory(container);
+			f.CreateBuilder(serviceProvider);
+
+			container.Register(
+				// Component.For<IUserService>().ImplementedBy<UserService>().LifestyleNetTransient(),
+				Classes.FromThisAssembly().BasedOn<IUserService>().WithServiceAllInterfaces().LifestyleNetStatic()
+				);
+
+			TaskCompletionSource<IUserService> tcs = new TaskCompletionSource<IUserService>();
+
+			ThreadPool.UnsafeQueueUserWorkItem(state => {
+				IUserService actualUserService = null;
+				try {
+					// creating a service provider here will be troublesome too
+					IServiceProvider sp = f.CreateServiceProvider(container);
+
+					actualUserService = sp.GetService<IUserService>();
 					Assert.NotNull(actualUserService);
 				}
 				catch (Exception ex) {
